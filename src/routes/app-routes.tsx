f355a025366357e6386549352711cd18d048e1cb
Veljko <radisavljevicveljko123@gmail.com>
2025-04-25 16:15:35 +0200

    add settings pages

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -2,7 +2,11 @@ import DraftsPage from '@dashboard-pages/drafts-page'
 import GuidesPage from '@dashboard-pages/guides-page'
 import HelpAndSupportPage from '@dashboard-pages/help-and-support-page'
 import InboxPage from '@dashboard-pages/inbox-page'
+import IntegrationsPage from '@dashboard-pages/integrations-page'
 import IssuesPage from '@dashboard-pages/issues-page'
+import PreferencesPage from '@dashboard-pages/preferences-page'
+import ProfilePage from '@dashboard-pages/profile-page'
+import SecurityPage from '@dashboard-pages/security-page'
 import DashboardLayout from '@layouts/dashboard-layout'
 import SettingsLayout from '@layouts/settings-layout'
 import { Route, Routes } from 'react-router'
@@ -16,7 +20,12 @@ export default function AppRoutes() {
         <Route path="drafts" element={<DraftsPage />} />
         <Route path="guides" element={<GuidesPage />} />
         <Route path="help-and-support" element={<HelpAndSupportPage />} />
-        <Route path="settings" element={<SettingsLayout />} />
+        <Route path="settings" element={<SettingsLayout />}>
+          <Route index element={<ProfilePage />} />
+          <Route path="security" element={<SecurityPage />} />
+          <Route path="preferences" element={<PreferencesPage />} />
+          <Route path="integrations" element={<IntegrationsPage />} />
+        </Route>
       </Route>
     </Routes>
   )
