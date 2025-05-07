f7fca5c510267f9558a1896b61e3b8be781c1955
Veljko <radisavljevicveljko123@gmail.com>
2025-05-07 13:56:47 +0200

    add shadow to switch

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -6,7 +6,7 @@ export default function Switch({ className, ...props }: SwitchProps) {
   return (
     <input
       className={cn(
-        'checked:bg-brand-500 relative aspect-2/1 w-(--switch-width) appearance-none rounded-full bg-neutral-800 before:absolute before:top-1/2 before:left-0.5 before:size-3 before:-translate-y-1/2 before:rounded-full before:bg-white before:transition before:duration-100 before:ease-in-out checked:before:left-0 checked:before:translate-x-[150%] hover:cursor-pointer',
+        'checked:bg-brand-500 relative aspect-2/1 w-(--switch-width) appearance-none rounded-full bg-neutral-800 shadow-sm before:absolute before:top-1/2 before:left-0.5 before:size-3 before:-translate-y-1/2 before:rounded-full before:bg-white before:transition before:duration-100 before:ease-in-out checked:before:left-0 checked:before:translate-x-[150%] hover:cursor-pointer',
         className
       )}
       type="checkbox"
