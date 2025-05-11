39f66db80d389375f138eaa1a4b07b619ec906ba
Veljko <radisavljevicveljko123@gmail.com>
2025-05-11 13:23:58 +0200

    add active and hover state to chat card links

--- src/components/dashboard-components/chat-card.tsx
+++ src/components/dashboard-components/chat-card.tsx
@@ -1,5 +1,5 @@
 import { Chat } from '@utils/types'
-import { Link } from 'react-router'
+import { NavLink } from 'react-router'
 
 type ChatCardProps = {
   chat: Chat
@@ -12,13 +12,19 @@ export default function ChatCard({
   },
 }: ChatCardProps) {
   return (
-    <article className="border-b-section-outline flex items-center gap-2 border-b p-4">
+    <article className="relative isolate flex items-center gap-2 p-4 hover:bg-neutral-800 hover:bg-[image:linear-gradient(hsl(from_var(--color-black)_h_s_l_/_20%),hsl(from_var(--color-black)_h_s_l_/_20%))]">
       <div className="size-10 overflow-hidden rounded-full bg-neutral-700">
         <img src={image} alt={name} className="object-cover" />
       </div>
-      <Link to={`/dashboard/inbox/${id}`}>
+      <NavLink
+        to={`/dashboard/inbox/${id}`}
+        className={({ isActive }) =>
+          `text-clickable ${isActive ? 'before:absolute before:inset-0 before:-z-1 before:bg-neutral-800' : ''}`
+        }
+      >
+        <span className="absolute inset-0"></span>
         <h3>{name}</h3>
-      </Link>
+      </NavLink>
     </article>
   )
 }
--- src/components/dashboard-components/chat-cards-list.tsx
+++ src/components/dashboard-components/chat-cards-list.tsx
@@ -1,13 +1,24 @@
 import ChatCard from '@dashboard-components/chat-card'
 import { Chat } from '@utils/types'
+import { cn } from '@utils/utils'
 
 type ChatCardsListProps = React.ComponentPropsWithoutRef<'ul'> & {
   chats: Chat[]
 }
 
-export default function ChatCardsList({ chats, ...props }: ChatCardsListProps) {
+export default function ChatCardsList({
+  chats,
+  className,
+  ...props
+}: ChatCardsListProps) {
   return (
-    <ul {...props}>
+    <ul
+      className={cn(
+        'divide-section-outline border-b-section-outline divide-y border-b',
+        className
+      )}
+      {...props}
+    >
       {chats.map((chat) => (
         <li key={chat.id}>
           <ChatCard chat={chat} />
