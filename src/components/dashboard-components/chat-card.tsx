3e3ed2d64e7cc72ba07b095827a972cb3d7c576e
Veljko <radisavljevicveljko123@gmail.com>
2025-05-14 19:03:32 +0200

    change chat card active and hover backgrounds

--- src/components/dashboard-components/chat-card.tsx
+++ src/components/dashboard-components/chat-card.tsx
@@ -12,14 +12,14 @@ export default function ChatCard({
   },
 }: ChatCardProps) {
   return (
-    <article className="relative isolate flex items-center gap-2 p-4 hover:bg-neutral-800 hover:bg-[image:linear-gradient(hsl(from_var(--color-black)_h_s_l_/_20%),hsl(from_var(--color-black)_h_s_l_/_20%))]">
+    <article className="relative isolate flex items-center gap-2 p-4 hover:bg-neutral-800">
       <div className="size-10 overflow-hidden rounded-full bg-neutral-700">
         <img src={image} alt={name} className="object-cover" />
       </div>
       <NavLink
         to={`/dashboard/inbox/${id}`}
         className={({ isActive }) =>
-          `text-clickable ${isActive ? 'before:absolute before:inset-0 before:-z-1 before:bg-neutral-800' : ''}`
+          `text-clickable ${isActive ? 'before:absolute before:inset-0 before:-z-1 before:bg-neutral-700' : ''}`
         }
       >
         <span className="absolute inset-0"></span>
