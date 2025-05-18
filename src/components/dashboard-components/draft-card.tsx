add7dc56cda7e227115300c0f73bab8bfcd39333
Veljko <radisavljevicveljko123@gmail.com>
2025-05-18 08:56:34 +0200

    link draft card to draft page

--- src/components/dashboard-components/draft-card.tsx
+++ src/components/dashboard-components/draft-card.tsx
@@ -1,19 +1,25 @@
 import Divider from '@dashboard-components/ui/divider'
 import TimeAgo from '@dashboard-components/ui/time-ago'
 import { Draft } from '@utils/types'
+import { Link } from 'react-router'
 
 type DraftCardProps = {
   draft: Draft
 }
 
 export default function DraftCard({
-  draft: { title, lastEdit },
+  draft: { id, title, lastEdit },
 }: DraftCardProps) {
   return (
-    <article className="border-section-outline h-full overflow-hidden rounded-lg border shadow-sm">
+    <article className="border-section-outline relative h-full overflow-hidden rounded-lg border shadow-sm">
       <div className="bg-clickable/10 aspect-3/1 w-full"></div>
       <div className="p-2">
-        <h3 className="line-clamp-2 text-balance">{title}</h3>
+        <h3 className="line-clamp-2 text-balance">
+          <Link to={`/dashboard/drafts/${id}`}>
+            <span className="absolute inset-0"></span>
+            {title}
+          </Link>
+        </h3>
       </div>
       <Divider />
       <div className="p-2">
