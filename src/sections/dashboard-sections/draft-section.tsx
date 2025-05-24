a9627eed705f08529da5b771a23c74fa8d0b8c6f
Veljko <radisavljevicveljko123@gmail.com>
2025-05-24 11:33:26 +0200

    add renaming draft title functionality in draft section

--- src/sections/dashboard-sections/draft-section.tsx
+++ src/sections/dashboard-sections/draft-section.tsx
@@ -1,6 +1,7 @@
 import Divider from '@dashboard-components/ui/divider'
 import { useDrafts } from '@services/contexts/drafts-context'
 import { TITLE_PREFIX } from '@utils/constants'
+import { useRef, useState } from 'react'
 import { Link, Navigate } from 'react-router'
 
 type DraftSectionProps = {
@@ -8,13 +9,16 @@ type DraftSectionProps = {
 }
 
 export default function DraftSection({ draftId }: DraftSectionProps) {
-  const { getDraftById } = useDrafts()
+  const { getDraftById, renameDraft } = useDrafts()
 
   const draft = getDraftById(draftId)
 
   if (!draft) return <Navigate to="/dashboard/drafts" />
 
   const { title } = draft
+
+  const [newTitle, setNewTitle] = useState(title)
+  const inputRef = useRef<HTMLInputElement>(null)
 
   return (
     <section>
@@ -25,7 +29,20 @@ export default function DraftSection({ draftId }: DraftSectionProps) {
             Drafts
           </Link>
           <span className="text-neutral-400">/</span>
-          <p>{title}</p>
+          <input
+            ref={inputRef}
+            value={newTitle}
+            onChange={(e) => setNewTitle(e.target.value)}
+            onBlur={() => renameDraft(draftId, newTitle)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                renameDraft(draftId, newTitle)
+                inputRef.current?.blur()
+              }
+            }}
+            className="text-clickable w-full max-w-85"
+            maxLength={80}
+          />
         </div>
       </header>
       <Divider />
