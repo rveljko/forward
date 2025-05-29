efd43aebc9418ba3fab551b58c2d1c013546ba37
Veljko <radisavljevicveljko123@gmail.com>
2025-05-29 09:08:39 +0200

    refactor hiding logo text logic

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -10,7 +10,9 @@ export default function Logo({ hideText }: LogoProps) {
       <span className="shrink-0">
         <LogoIcon className="[--clip-path:_polygon(0_0,_100%_0,_100%_100%,_0_100%)] group-hover:[&_rect]:[clip-path:var(--clip-path)] pointer-coarse:group-active:[&_rect]:[clip-path:var(--clip-path)]" />
       </span>
-      {!hideText && <span className="text-xl font-semibold">Forward</span>}
+      <span className={`text-xl font-semibold ${hideText ? 'hidden' : ''}`}>
+        Forward
+      </span>
     </div>
   )
 }
