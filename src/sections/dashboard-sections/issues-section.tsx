0d6e533fa5ae7f22226070b5b23cf013226e96df
Veljko <radisavljevicveljko123@gmail.com>
2025-05-30 11:49:47 +0200

    add header to issue section

--- src/sections/dashboard-sections/issues-section.tsx
+++ src/sections/dashboard-sections/issues-section.tsx
@@ -1,3 +1,17 @@
+import Divider from '@dashboard-components/ui/divider'
+import SidebarClosedIcon from '@icons/sidebar-closed-icon'
+import Button from '@ui/button'
+
 export default function IssuesSection() {
-  return <section>issues-section</section>
+  return (
+    <section>
+      <header className="flex items-center justify-between p-4">
+        <h1>Issues</h1>
+        <Button variant="tertiary" className="p-0.5">
+          <SidebarClosedIcon />
+        </Button>
+      </header>
+      <Divider />
+    </section>
+  )
 }
