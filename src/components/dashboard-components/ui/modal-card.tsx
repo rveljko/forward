dd9dea57feec5e37ab9c91c3e4d8a28800a72112
Veljko <radisavljevicveljko123@gmail.com>
2025-06-09 11:07:39 +0200

    apply modal card border radius based on preferences

--- src/components/dashboard-components/ui/modal-card.tsx
+++ src/components/dashboard-components/ui/modal-card.tsx
@@ -1,12 +1,16 @@
+import { usePreferences } from '@services/contexts/preferences-context'
 import { cn } from '@utils/utils'
 
 type ModalCardProps = React.ComponentPropsWithoutRef<'article'>
 
 export default function ModalCard({ children, className }: ModalCardProps) {
+  const { getRemCornerRoundness } = usePreferences()
+
   return (
     <article
+      style={{ borderRadius: getRemCornerRoundness() }}
       className={cn(
-        'bg-modal-background border-section-outline w-full rounded-2xl border',
+        'bg-modal-background border-section-outline w-full border',
         className
       )}
     >
