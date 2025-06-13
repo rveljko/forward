e3685bf462b9699b3e1b440bd2e555e7d7c09470
Veljko <radisavljevicveljko123@gmail.com>
2025-06-13 16:42:04 +0200

    add role to divider

--- src/components/dashboard-components/ui/divider.tsx
+++ src/components/dashboard-components/ui/divider.tsx
@@ -2,7 +2,7 @@ type DividerProps = React.ComponentPropsWithoutRef<'div'>
 
 export default function Divider({ ...props }: DividerProps) {
   return (
-    <div {...props}>
+    <div role="separator" {...props}>
       <div className="h-0.25 w-full bg-black/40"></div>
       <div className="h-0.25 w-full bg-white/10"></div>
     </div>
