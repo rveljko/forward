a580ae0ccf4f57a29d57d99ccd7efb74394f09a0
Veljko <radisavljevicveljko123@gmail.com>
2025-06-19 12:59:21 +0200

    fix dropdown checkbox type

--- src/components/dashboard-components/ui/dropdown.tsx
+++ src/components/dashboard-components/ui/dropdown.tsx
@@ -199,7 +199,7 @@ type CheckboxProps = React.ComponentPropsWithoutRef<'input'>
 function Checkbox({ className, ...props }: CheckboxProps) {
   return (
     <input
-      type="radio"
+      type="checkbox"
       className={cn(
         "before:inset-ring-clickable/20 hover:before:inset-ring-brand-500 pointer-coarse:active:before:inset-ring-brand-500 checked:before:bg-brand-500 relative size-3.5 appearance-none before:absolute before:flex before:size-full before:items-center before:justify-center before:rounded-xs before:text-xs before:text-white before:inset-ring checked:before:inset-ring-0 checked:before:content-['✓']",
         className
