79744cae4d9b21e873d638e37fccdb6e97d90616
Veljko <radisavljevicveljko123@gmail.com>
2025-07-05 08:13:25 +0200

    encapsulate radio button

---
+++ src/components/dashboard-components/ui/radio-button.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@utils/utils'
+
+type RadioButtonProps = React.ComponentPropsWithoutRef<'input'>
+
+export default function RadioButton({ className, ...props }: RadioButtonProps) {
+  return (
+    <input
+      type="radio"
+      className={cn(
+        'before:inset-ring-clickable/20 pointer-coarse:active:before:inset-ring-brand-500 hover:before:inset-ring-brand-500 checked:before:inset-ring-brand-500 relative size-3.5 appearance-none before:absolute before:size-full before:rounded-full before:inset-ring checked:before:inset-ring-4',
+        className
+      )}
+      {...props}
+    />
+  )
+}
