5afa85680f256d19d438c8efd05af0cb8270ed23
Veljko <radisavljevicveljko123@gmail.com>
2025-07-07 13:24:04 +0200

    change divider light mode color

--- src/components/dashboard-components/ui/divider.tsx
+++ src/components/dashboard-components/ui/divider.tsx
@@ -1,10 +1,16 @@
+import { usePreferences } from '@services/contexts/preferences-context'
+
 type DividerProps = React.ComponentPropsWithoutRef<'div'>
 
 export default function Divider({ ...props }: DividerProps) {
+  const { isLightTheme } = usePreferences()
+
   return (
     <div role="separator" {...props}>
-      <div className="h-0.25 w-full bg-black/40"></div>
-      <div className="h-0.25 w-full bg-white/10"></div>
+      <div
+        className={`h-0.25 w-full ${isLightTheme ? 'bg-transparent' : 'bg-black/40'}`}
+      />
+      <div className="bg-section-outline h-0.25 w-full" />
     </div>
   )
 }
