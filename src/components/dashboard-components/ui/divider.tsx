f8badb82beef47deddb4cc88e7a58d8807b3e7e2
Veljko <radisavljevicveljko123@gmail.com>
2025-07-08 11:21:15 +0200

    create css variables for divider backgrounds

--- src/components/dashboard-components/ui/divider.tsx
+++ src/components/dashboard-components/ui/divider.tsx
@@ -1,16 +1,10 @@
-import { usePreferences } from '@services/contexts/preferences-context'
-
 type DividerProps = React.ComponentPropsWithoutRef<'div'>
 
 export default function Divider({ ...props }: DividerProps) {
-  const { isLightTheme } = usePreferences()
-
   return (
     <div role="separator" {...props}>
-      <div
-        className={`h-0.25 w-full ${isLightTheme ? 'bg-transparent' : 'bg-black/40'}`}
-      />
-      <div className="bg-section-outline h-0.25 w-full" />
+      <div className="bg-divider-top-background h-0.25 w-full" />
+      <div className="bg-divider-bottom-background h-0.25 w-full" />
     </div>
   )
 }
