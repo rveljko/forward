fa22969dd08b0d997f00a2642741a0139369f03e
Veljko <radisavljevicveljko123@gmail.com>
2025-07-14 12:45:19 +0200

    animate project information modal

--- src/components/dashboard-components/project-information-modal-button.tsx
+++ src/components/dashboard-components/project-information-modal-button.tsx
@@ -21,7 +21,24 @@ export default function ProjectInformationModalButton({
     >
       <Modal isOpened={isOpened} closeModal={closeModal}>
         <Modal.Overlay className="md:items-end">
-          <Modal.Dialog className="md:h-full">
+          <Modal.Dialog
+            initial={{
+              scale: 'var(--scale-from)',
+              translateY: 'var(--slide-y-from)',
+              translateX: 'var(--slide-x-from)',
+            }}
+            animate={{
+              scale: 'var(--scale-to)',
+              translateY: 'var(--slide-y-to)',
+              translateX: 'var(--slide-x-to)',
+            }}
+            exit={{
+              scale: 'var(--scale-from)',
+              translateY: 'var(--slide-y-from)',
+              translateX: 'var(--slide-x-from)',
+            }}
+            className="[--slide-x-from:--spacing(0)] [--slide-x-to:--spacing(0)] md:h-full md:[--scale-from:100%] md:[--slide-x-from:--spacing(20)] md:[--slide-y-from:--spacing(0)]"
+          >
             <Modal.FocusLock className="md:h-full">
               <ProjectInformationModal
                 closeModal={closeModal}
