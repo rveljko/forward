abc97124aa5eae451075f9132d416baf381f5c66
Veljko <radisavljevicveljko123@gmail.com>
2025-07-16 10:13:30 +0200

    add transition to radio button

--- src/components/dashboard-components/ui/radio-button.tsx
+++ src/components/dashboard-components/ui/radio-button.tsx
@@ -7,7 +7,7 @@ export default function RadioButton({ className, ...props }: RadioButtonProps) {
     <input
       type="radio"
       className={cn(
-        'before:inset-ring-clickable/20 pointer-coarse:active:before:inset-ring-brand-500 hover:before:inset-ring-brand-500 checked:before:inset-ring-brand-500 relative size-3.5 appearance-none before:absolute before:size-full before:rounded-full before:inset-ring checked:before:inset-ring-4',
+        'before:inset-ring-clickable/20 pointer-coarse:active:before:inset-ring-brand-500 hover:before:inset-ring-brand-500 checked:before:inset-ring-brand-500 relative size-3.5 appearance-none before:absolute before:size-full before:rounded-full before:inset-ring before:transition-all checked:before:inset-ring-4',
         className
       )}
       {...props}
