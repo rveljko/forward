4ad4099ab46e04ad43807195f815da35fe37478e
Veljko <radisavljevicveljko123@gmail.com>
2025-07-24 13:57:51 +0200

    increase header backdrop blur

--- src/components/landing-page-components/header.tsx
+++ src/components/landing-page-components/header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
     <div className="fixed top-0 z-999 w-full">
       <header
         data-opened={isOpened}
-        className="group border-b-section-outline relative h-(--header-height) border-b before:absolute before:inset-0 before:backdrop-blur-xs"
+        className="group border-b-section-outline relative h-(--header-height) border-b before:absolute before:inset-0 before:backdrop-blur-sm"
       >
         <Container className="relative z-998 flex items-center justify-between py-4">
           <div className="md:flex-1/3">
@@ -43,7 +43,7 @@ export default function Header() {
             <span className="sr-only">Toggle Menu</span>
           </Button>
           <nav
-            className={`max-md:border-section-outline [--opacity-from:0%] [--opacity-to:100%] [--scale-from:95%] [--scale-to:100%] [--slide-y-from:--spacing(10)] [--slide-y-to:--spacing(0)] max-md:absolute max-md:inset-x-0 max-md:top-[calc(var(--header-height)+(--spacing(4)))] max-md:z-997 max-md:rounded-2xl max-md:border max-md:py-4 max-md:backdrop-blur-xs max-md:transition-all max-md:transition-discrete max-md:duration-300 md:flex-2/3 md:items-center md:gap-2 max-md:starting:translate-y-(--slide-y-from) max-md:starting:scale-(--scale-from) max-md:starting:opacity-(--opacity-from) ${isOpened ? 'max-md:translate-y-(--slide-y-to) max-md:scale-(--scale-to) max-md:opacity-(--opacity-to)' : 'max-md:hidden max-md:translate-y-(--slide-y-from) max-md:scale-(--scale-from) max-md:opacity-(--opacity-from)'}`}
+            className={`max-md:border-section-outline [--opacity-from:0%] [--opacity-to:100%] [--scale-from:95%] [--scale-to:100%] [--slide-y-from:--spacing(10)] [--slide-y-to:--spacing(0)] max-md:absolute max-md:inset-x-0 max-md:top-[calc(var(--header-height)+(--spacing(4)))] max-md:z-997 max-md:rounded-2xl max-md:border max-md:py-4 max-md:backdrop-blur-sm max-md:transition-all max-md:transition-discrete max-md:duration-300 md:flex-2/3 md:items-center md:gap-2 max-md:starting:translate-y-(--slide-y-from) max-md:starting:scale-(--scale-from) max-md:starting:opacity-(--opacity-from) ${isOpened ? 'max-md:translate-y-(--slide-y-to) max-md:scale-(--scale-to) max-md:opacity-(--opacity-to)' : 'max-md:hidden max-md:translate-y-(--slide-y-from) max-md:scale-(--scale-from) max-md:opacity-(--opacity-from)'}`}
           >
             <ul className="flex max-md:flex-col md:items-center">
               <li>
