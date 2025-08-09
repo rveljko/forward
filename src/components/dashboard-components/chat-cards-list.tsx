82aa533e6faaf110275b8d23ad32a07f9aead5df
Veljko <radisavljevicveljko123@gmail.com>
2025-08-09 12:06:08 +0200

    animate chat cards

--- src/components/dashboard-components/chat-cards-list.tsx
+++ src/components/dashboard-components/chat-cards-list.tsx
@@ -1,29 +1,21 @@
 import ChatCard from '@dashboard-components/chat-card'
 import { Chat } from '@utils/types'
-import { cn } from '@utils/utils'
+import { AnimatePresence, motion } from 'motion/react'
 
 type ChatCardsListProps = React.ComponentPropsWithoutRef<'ul'> & {
   chats: Chat[]
 }
 
-export default function ChatCardsList({
-  chats,
-  className,
-  ...props
-}: ChatCardsListProps) {
+export default function ChatCardsList({ chats, ...props }: ChatCardsListProps) {
   return (
-    <ul
-      className={cn(
-        'divide-section-outline border-b-section-outline divide-y border-b',
-        className
-      )}
-      {...props}
-    >
-      {chats.map((chat) => (
-        <li key={chat.id}>
-          <ChatCard chat={chat} />
-        </li>
-      ))}
+    <ul {...props}>
+      <AnimatePresence>
+        {chats.map((chat) => (
+          <motion.li key={chat.id} layout>
+            <ChatCard chat={chat} />
+          </motion.li>
+        ))}
+      </AnimatePresence>
     </ul>
   )
 }
