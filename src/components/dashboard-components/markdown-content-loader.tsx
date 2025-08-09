ad7f6022761d006341e905fe1a6cbea9992a8797
Veljko <radisavljevicveljko123@gmail.com>
2025-08-09 08:07:18 +0200

    fix markdown content loader spacing

--- src/components/dashboard-components/markdown-content-loader.tsx
+++ src/components/dashboard-components/markdown-content-loader.tsx
@@ -11,7 +11,10 @@ export default function MarkdownContentLoader({
     <Markdown
       components={{
         h1: ({ node, ...props }) => (
-          <h1 className="mb-2 text-2xl font-medium md:text-3xl" {...props} />
+          <h1
+            className="mb-2 text-2xl font-medium has-[+_h2]:mb-8 md:text-3xl"
+            {...props}
+          />
         ),
         h2: ({ node, ...props }) => (
           <h2 className="mb-2 text-xl font-medium md:text-2xl" {...props} />
@@ -22,12 +25,20 @@ export default function MarkdownContentLoader({
         h4: ({ node, ...props }) => (
           <h4 className="mb-2 text-base font-medium md:text-lg" {...props} />
         ),
-        p: ({ node, ...props }) => <p className="mb-2" {...props} />,
+        p: ({ node, ...props }) => (
+          <p className="mb-2 has-[+_h2]:mb-8 has-[+_h3]:mb-8" {...props} />
+        ),
         ul: ({ node, ...props }) => (
-          <ul className="mb-2 ml-6 list-disc space-y-1.5" {...props} />
+          <ul
+            className="mb-2 ml-6 list-disc space-y-1.5 has-[+_h2]:mb-8 has-[+_h3]:mb-8"
+            {...props}
+          />
         ),
         ol: ({ node, ...props }) => (
-          <ol className="mb-2 ml-6 list-decimal space-y-1.5" {...props} />
+          <ol
+            className="mb-2 ml-6 list-decimal space-y-1.5 has-[+_h2]:mb-8 has-[+_h3]:mb-8"
+            {...props}
+          />
         ),
         li: ({ node, ...props }) => (
           <li
