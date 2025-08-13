94cb0685ca076b5f13d2f67d903affc7dfcb0753
Veljko <radisavljevicveljko123@gmail.com>
2025-08-13 11:25:08 +0200

    change heading level in help and supprt header

--- src/components/dashboard-components/help-and-support-header.tsx
+++ src/components/dashboard-components/help-and-support-header.tsx
@@ -1,7 +1,7 @@
 export default function HelpAndSupportHeader() {
   return (
     <header>
-      <h2 className="mb-1">Help and Support</h2>
+      <h1 className="mb-1">Help and Support</h1>
       <p>Get assistance and provide feedback</p>
     </header>
   )
