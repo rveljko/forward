96a49c03de8e6730a450eadde21e9ca866b64e58
Veljko <radisavljevicveljko123@gmail.com>
2026-01-23 13:24:49 +0100

    change touch device active background color in issues list item

--- src/components/dashboard-components/issues-list-item.tsx
+++ src/components/dashboard-components/issues-list-item.tsx
@@ -58,7 +58,7 @@ export default function IssuesListItem({
     return (
       <article
         className={cn(
-          'bg-clickable/5 ring-section-outline h-15.5 ring',
+          'bg-issues-list-item-active-background ring-section-outline h-15.5 ring',
           className
         )}
         style={style}
@@ -70,7 +70,7 @@ export default function IssuesListItem({
   return (
     <article
       className={cn(
-        `bg-section-background-color pointer-coarse:active:bg-clickable/5 relative flex items-center justify-between gap-2 p-4 transition-[background-color] ${activeIssueId === id ? 'bg-issues-list-item-active-background' : 'bg-section-background-color'}`,
+        `bg-section-background-color pointer-coarse:active:bg-issues-list-item-active-background relative flex items-center justify-between gap-2 p-4 transition-[background-color] ${activeIssueId === id ? 'bg-issues-list-item-active-background' : 'bg-section-background-color'}`,
         className
       )}
       style={style}
