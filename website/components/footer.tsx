bf08427ce1d776d6cf3ec173ee1c377f3a133a49
Veljko <radisavljevicveljko123@gmail.com>
2026-02-17 10:55:16 +0100

    add footer

---
+++ website/components/footer.tsx
@@ -0,0 +1,62 @@
+import Logo from '@/components/ui/logo'
+import Link from 'next/link'
+
+export default function Footer() {
+  return (
+    <footer className="mt-auto flex flex-col gap-6 border-t border-t-neutral-200 p-6 md:gap-8 md:p-8">
+      <Link href="/" className="flex w-max">
+        <Logo />
+      </Link>
+      <div className="w-full border-b border-dashed border-b-neutral-200" />
+      <nav className="flex flex-wrap gap-8 md:gap-16">
+        <ul className="flex flex-col gap-1">
+          <li className="text-neutral-600">Products</li>
+          <li>
+            <Link href="/">Tasks</Link>
+          </li>
+          <li>
+            <Link href="/">Issues</Link>
+          </li>
+          <li>
+            <Link href="/">Drafts</Link>
+          </li>
+          <li>
+            <Link href="/">Integrations</Link>
+          </li>
+        </ul>
+        <ul className="flex flex-col gap-1">
+          <li className="text-neutral-600">Company</li>
+          <li>
+            <Link href="/">Security</Link>
+          </li>
+          <li>
+            <Link href="/">Customers</Link>
+          </li>
+        </ul>
+        <ul className="flex flex-col gap-1">
+          <li className="text-neutral-600">Resources</li>
+          <li>
+            <Link href="/">Help Center</Link>
+          </li>
+          <li>
+            <Link href="/">Guides</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="w-full border-b border-dashed border-b-neutral-200" />
+      <div className="flex flex-wrap justify-between gap-8">
+        <p className="text-neutral-600">
+          &copy; {new Date().getFullYear()} All rights reserved
+        </p>
+        <ul className="flex flex-wrap gap-2">
+          <li>
+            <Link href="/">Privacy Policy</Link>
+          </li>
+          <li>
+            <Link href="/">Terms of Service</Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  )
+}
--- website/components/layout-pattern.tsx
+++ website/components/layout-pattern.tsx
@@ -1,3 +1,5 @@
+import Footer from '@/components/footer'
+
 type LayoutPatternProps = React.ComponentPropsWithoutRef<'div'> & {
   children: React.ReactNode
 }
@@ -10,9 +12,12 @@ export default function LayoutPattern({
   return (
     <div className="mx-auto flex w-[calc(100%-(--spacing(8)))] max-w-335.5 grow pt-(--header-height)">
       <div className="min-w-25 border-l border-l-neutral-200 bg-[repeating-linear-gradient(-45deg,var(--color-neutral-200)_0,var(--color-neutral-200)_1px,transparent_0,transparent_50%)] bg-size-[--spacing(2.5)_--spacing(2.5)] bg-fixed max-md:hidden"></div>
-      <main className="@container w-full divide-y divide-neutral-200 border-x border-x-neutral-200 *:last:border-b *:last:border-b-neutral-200">
-        {children}
-      </main>
+      <div className="@container flex min-h-full w-full flex-col border-x border-x-neutral-200">
+        <main className="divide-y divide-neutral-200 *:last:mb-6 *:last:border-b *:last:border-b-neutral-200 md:*:last:mb-8">
+          {children}
+        </main>
+        <Footer />
+      </div>
       <div className="min-w-25 border-r border-r-neutral-200 bg-[repeating-linear-gradient(45deg,var(--color-neutral-200)_0,var(--color-neutral-200)_1px,transparent_0,transparent_50%)] bg-size-[--spacing(2.5)_--spacing(2.5)] bg-fixed max-md:hidden"></div>
     </div>
   )
