4e47a269a949f6428d7550fa76575793a51ebe5f
Veljko <radisavljevicveljko123@gmail.com>
2026-02-19 11:58:35 +0100

    rename customers folder to customers logos

--- website/data/customers.ts
+++ website/data/customers.ts
@@ -1,9 +1,9 @@
-import AticaLogo from '@/components/customers/atica-logo'
-import GoldlineLogo from '@/components/customers/goldline-logo'
-import HooksLogo from '@/components/customers/hooks-logo'
-import KyanLogo from '@/components/customers/kyan-logo'
-import LightAiLogo from '@/components/customers/light-ai-logo'
-import LivaLogo from '@/components/customers/liva-logo'
+import AticaLogo from '@/components/customers-logos/atica-logo'
+import GoldlineLogo from '@/components/customers-logos/goldline-logo'
+import HooksLogo from '@/components/customers-logos/hooks-logo'
+import KyanLogo from '@/components/customers-logos/kyan-logo'
+import LightAiLogo from '@/components/customers-logos/light-ai-logo'
+import LivaLogo from '@/components/customers-logos/liva-logo'
 
 type Customer = {
   name: string
