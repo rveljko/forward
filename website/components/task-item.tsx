804c651f3542de300aa9f934b6350f6817c806d8
Veljko <radisavljevicveljko123@gmail.com>
2026-02-21 17:23:39 +0100

    change font weight in task item

--- website/components/task-item.tsx
+++ website/components/task-item.tsx
@@ -9,7 +9,7 @@ export default function TaskItem({ task }: TaskItemProps) {
     <article className="flex justify-between gap-2 border-b border-black/10 p-4">
       <div className="flex items-center gap-2">
         <span className="size-3.5 shrink-0 rounded-sm inset-ring inset-ring-black/20" />
-        <h3 className="line-clamp-1 text-sm break-all">{task}</h3>
+        <h3 className="line-clamp-1 text-sm font-medium break-all">{task}</h3>
       </div>
       <div className="[&_svg]:text-dashboard-neutral-600 flex items-center gap-2">
         <span className="text-dashboard-neutral-600 text-xs text-nowrap">
