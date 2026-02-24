2f02b66f0ffa3329deae3da0604704aaf93c1f78
Veljko <radisavljevicveljko123@gmail.com>
2026-02-24 13:49:38 +0100

    add create new issue modal to dashboard in issues section

--- website/sections/issues-section.tsx
+++ website/sections/issues-section.tsx
@@ -6,8 +6,11 @@ import ArrowsSortIcon from '@/icons/arrows-sort-icon'
 import CircleDashedIcon from '@/icons/circle-dashed-icon'
 import CircleEmptyIcon from '@/icons/circle-empty-icon'
 import CircleHalfIcon from '@/icons/circle-half-icon'
+import CloseIcon from '@/icons/close-icon'
+import DesignIssueTagIcon from '@/icons/design-issue-tag-icon'
 import FilterIcon from '@/icons/filters-icon'
 import PlusIcon from '@/icons/plus-icon'
+import PriorityNoPriorityIcon from '@/icons/priority-no-priority-icon'
 import Section from '@/sections/section'
 
 export default function IssuesSection() {
@@ -149,6 +152,58 @@ function DashboardPanel() {
             className="size-full overflow-hidden rounded-lg border border-black/10 bg-white"
           >
             <div className="absolute inset-0 rounded-b-lg bg-black/5" />
+            <div className="absolute inset-4 flex flex-col items-center justify-end md:justify-start">
+              <div className="w-full max-w-100 rounded-lg bg-white shadow-sm ring ring-black/10">
+                <div className="p-4">
+                  <div className="flex justify-between gap-2">
+                    <span className="text-dashboard-neutral-600 inline-block text-lg">
+                      Issue title
+                    </span>
+                    <CloseIcon />
+                  </div>
+                  <div className="h-18 w-full">
+                    <span className="text-dashboard-neutral-600 text-sm">
+                      Issue description
+                    </span>
+                  </div>
+                </div>
+                <div className="scrollbar-hidden flex items-center gap-2 overflow-x-auto p-4 pt-px">
+                  <span className="text flex w-max items-center gap-1 rounded-md px-1.5 py-1 text-sm text-nowrap shadow-sm ring ring-black/10">
+                    <CircleDashedIcon />
+                    Backlog
+                  </span>
+                  <span className="text flex w-max items-center gap-1 rounded-md px-1.5 py-1 text-sm text-nowrap shadow-sm ring ring-black/10">
+                    <PriorityNoPriorityIcon />
+                    No Priority
+                  </span>
+                  <span className="text flex w-max items-center gap-1 rounded-md px-1.5 py-1 text-sm text-nowrap shadow-sm ring ring-black/10">
+                    <DesignIssueTagIcon />
+                    Design
+                  </span>
+                </div>
+                <div
+                  dir="rtl"
+                  className="scrollbar-hidden flex overflow-x-auto border-t border-t-black/10 p-4"
+                >
+                  <div dir="ltr" className="ml-auto flex items-center gap-2">
+                    <div className="flex items-center gap-1">
+                      <span className="text-dashboard-neutral-600 text-sm text-nowrap">
+                        Create more
+                      </span>
+                      <div className="bg-dashboard-neutral-300 flex aspect-2/1 w-7 rounded-full p-0.5 shadow-sm">
+                        <div className="aspect-square h-full rounded-full bg-white" />
+                      </div>
+                    </div>
+                    <span className="flex w-max items-center rounded-md px-2 py-1.5 text-sm text-nowrap shadow-sm ring ring-black/10">
+                      Cancel
+                    </span>
+                    <span className="bg-dashboard-brand-500 flex w-max items-center rounded-md px-2 py-1.5 text-sm text-nowrap text-white shadow-sm">
+                      Create New Issue
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
             <div className="border-b border-b-black/10 p-4 text-sm font-medium">
               Issues
             </div>
