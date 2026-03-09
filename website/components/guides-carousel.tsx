97279f65f5e25b0343f4c228a4e29acbd9c1f74d
Veljko <radisavljevicveljko123@gmail.com>
2026-03-09 19:14:14 +0100

    refactor guides carousel

---
+++ website/components/guides-carousel.tsx
@@ -0,0 +1,130 @@
+'use client'
+
+import Badge from '@/components/ui/badge'
+import CalendarIcon from '@/icons/calendar-icon'
+import { dayMonthShortFormatter } from '@/utils/date-formatters'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+
+type Guide = {
+  title: string
+  image: string
+  tags: {
+    text: string
+    color: React.ComponentProps<typeof Badge>['color']
+  }[]
+}
+
+const guides: Guide[] = [
+  {
+    title: 'How to Create a New Task',
+    image: '/images/how-to-create-a-task-thumbnail.png',
+    tags: [{ text: 'Tasks', color: 'blue' }],
+  },
+  {
+    title: 'How to Create a New Issue',
+    image: '/images/how-to-create-an-issue-thumbnail.png',
+    tags: [{ text: 'Issues', color: 'cyan' }],
+  },
+  {
+    title: 'How to Create a New Draft',
+    image: '/images/how-to-create-a-draft-thumbnail.png',
+    tags: [{ text: 'Drafts', color: 'violet' }],
+  },
+  {
+    title: 'How to Update User Information',
+    image: '/images/how-to-update-user-information-thumbnail.png',
+    tags: [
+      { text: 'Settings', color: 'sky' },
+      { text: 'Profile', color: 'pink' },
+    ],
+  },
+  {
+    title: 'How to Change Password',
+    image: '/images/how-to-change-password-thumbnail.png',
+    tags: [
+      { text: 'Settings', color: 'sky' },
+      { text: 'Security', color: 'orange' },
+    ],
+  },
+  {
+    title: 'How to Change Preferences',
+    image: '/images/how-to-change-preferences-thumbnail.png',
+    tags: [
+      { text: 'Settings', color: 'sky' },
+      { text: 'Preferences', color: 'green' },
+    ],
+  },
+  {
+    title: 'How to Toggle Integrations',
+    image: '/images/how-to-toggle-integrations-thumbnail.png',
+    tags: [
+      { text: 'Settings', color: 'sky' },
+      { text: 'Integrations', color: 'rose' },
+    ],
+  },
+]
+
+export default function GuidesCarousel() {
+  const [activeIndex, setActiveIndex] = useState(3)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((prev) => (prev + 1) % guides.length)
+    }, 2000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <ul className="relative size-full">
+      {guides.map(({ title, image, tags }, index) => (
+        <li key={index} className="absolute top-1/2 right-px -translate-y-1/2">
+          <article
+            data-is-before-previous={
+              activeIndex === index + 2 ||
+              (activeIndex === 0 && index === guides.length - 2) ||
+              (activeIndex === 1 && index === guides.length - 1)
+            }
+            data-is-previous={
+              activeIndex === index + 1 ||
+              (activeIndex === 0 && index === guides.length - 1)
+            }
+            data-is-active={activeIndex === index}
+            data-is-next={
+              activeIndex === index - 1 ||
+              (activeIndex === guides.length - 1 && index === 0)
+            }
+            className="w-full max-w-max translate-y-[calc(200%+(var(--gap)*2))] overflow-hidden rounded-lg bg-white opacity-0 shadow-sm ring ring-black/10 transition-transform duration-1000 [--gap:--spacing(4)] data-[is-active=true]:translate-0 data-[is-active=true]:opacity-100 data-[is-before-previous=true]:translate-y-[calc(-200%-(var(--gap)*2))] data-[is-before-previous=true]:opacity-100 data-[is-next=true]:translate-y-[calc(100%+var(--gap))] data-[is-next=true]:opacity-100 data-[is-previous=true]:translate-y-[calc(-100%-var(--gap))] data-[is-previous=true]:opacity-100"
+          >
+            <div className="flex flex-col gap-2 border-b border-b-neutral-300 p-2">
+              <div className="max-h-42 w-full max-w-75 overflow-hidden rounded-md border border-neutral-300 mask-linear-360 mask-linear-from-transparent mask-linear-to-black">
+                <Image
+                  src={image}
+                  alt=""
+                  width={1280}
+                  height={720}
+                  loading="lazy"
+                />
+              </div>
+              <h3 className="text-sm font-medium text-neutral-900">{title}</h3>
+              <ul className="flex items-center gap-1">
+                {tags.map(({ text, color }, index) => (
+                  <li key={index}>
+                    <Badge color={color}>{text}</Badge>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="p-2">
+              <span className="flex w-max items-center gap-1 text-xs text-neutral-600 [&_svg]:size-4">
+                <CalendarIcon />
+                {dayMonthShortFormatter(new Date())}
+              </span>
+            </div>
+          </article>
+        </li>
+      ))}
+    </ul>
+  )
+}
--- website/sections/guides-section.tsx
+++ website/sections/guides-section.tsx
@@ -1,69 +1,8 @@
+import GuidesCarousel from '@/components/guides-carousel'
 import Badge from '@/components/ui/badge'
 import Button from '@/components/ui/button'
 import ArrowRightIcon from '@/icons/arrow-right-icon'
-import CalendarIcon from '@/icons/calendar-icon'
 import Section from '@/sections/section'
-import { dayMonthShortFormatter } from '@/utils/date-formatters'
-import Image from 'next/image'
-
-type Guide = {
-  title: string
-  image: string
-  tags: {
-    text: string
-    color: React.ComponentProps<typeof Badge>['color']
-  }[]
-}
-
-const guides: Guide[] = [
-  {
-    title: 'How to Create a New Task',
-    image: '/images/how-to-create-a-task-thumbnail.png',
-    tags: [{ text: 'Tasks', color: 'blue' }],
-  },
-  {
-    title: 'How to Create a New Issue',
-    image: '/images/how-to-create-an-issue-thumbnail.png',
-    tags: [{ text: 'Issues', color: 'cyan' }],
-  },
-  {
-    title: 'How to Create a New Draft',
-    image: '/images/how-to-create-a-draft-thumbnail.png',
-    tags: [{ text: 'Drafts', color: 'violet' }],
-  },
-  {
-    title: 'How to Update User Information',
-    image: '/images/how-to-update-user-information-thumbnail.png',
-    tags: [
-      { text: 'Settings', color: 'sky' },
-      { text: 'Profile', color: 'pink' },
-    ],
-  },
-  {
-    title: 'How to Change Password',
-    image: '/images/how-to-change-password-thumbnail.png',
-    tags: [
-      { text: 'Settings', color: 'sky' },
-      { text: 'Security', color: 'orange' },
-    ],
-  },
-  {
-    title: 'How to Change Preferences',
-    image: '/images/how-to-change-preferences-thumbnail.png',
-    tags: [
-      { text: 'Settings', color: 'sky' },
-      { text: 'Preferences', color: 'green' },
-    ],
-  },
-  {
-    title: 'How to Toggle Integrations',
-    image: '/images/how-to-toggle-integrations-thumbnail.png',
-    tags: [
-      { text: 'Settings', color: 'sky' },
-      { text: 'Integrations', color: 'rose' },
-    ],
-  },
-]
 
 export default function GuidesSection() {
   return (
@@ -103,77 +42,8 @@ function TextPanel() {
 
 function GuidesCarouselPanel() {
   return (
-    <div className="scrollbar-hidden pointer-events-none flex h-100 w-full flex-col items-center overflow-hidden px-6 select-none md:px-8 @3xl:h-150 @3xl:items-end">
-      <ul className="animate-slide-y flex flex-col gap-4 pb-4">
-        {guides.map(({ title, image, tags }, index) => (
-          <li key={index}>
-            <article className="w-full max-w-max overflow-hidden rounded-lg bg-white shadow-sm ring ring-black/10">
-              <div className="flex flex-col gap-2 border-b border-b-neutral-300 p-2">
-                <div className="max-h-42 w-full max-w-75 overflow-hidden rounded-md border border-neutral-300 mask-linear-360 mask-linear-from-transparent mask-linear-to-black">
-                  <Image
-                    src={image}
-                    alt=""
-                    width={1280}
-                    height={720}
-                    loading="lazy"
-                  />
-                </div>
-                <h3 className="text-sm font-medium text-neutral-900">
-                  {title}
-                </h3>
-                <ul className="flex items-center gap-1">
-                  {tags.map(({ text, color }, index) => (
-                    <li key={index}>
-                      <Badge color={color}>{text}</Badge>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="p-2">
-                <span className="flex w-max items-center gap-1 text-xs text-neutral-600 [&_svg]:size-4">
-                  <CalendarIcon />
-                  {dayMonthShortFormatter(new Date())}
-                </span>
-              </div>
-            </article>
-          </li>
-        ))}
-      </ul>
-      <ul aria-hidden className="animate-slide-y flex flex-col gap-4 pb-4">
-        {guides.map(({ title, image, tags }, index) => (
-          <li key={index}>
-            <article className="w-full max-w-max overflow-hidden rounded-lg bg-white shadow-sm ring ring-black/10">
-              <div className="flex flex-col gap-2 border-b border-b-neutral-300 p-2">
-                <div className="max-h-42 w-full max-w-75 overflow-hidden rounded-md border border-neutral-300 mask-linear-360 mask-linear-from-transparent mask-linear-to-black">
-                  <Image
-                    src={image}
-                    alt=""
-                    width={1280}
-                    height={720}
-                    loading="lazy"
-                  />
-                </div>
-                <h3 className="text-sm font-medium text-neutral-900">
-                  {title}
-                </h3>
-                <ul className="flex items-center gap-1">
-                  {tags.map(({ text, color }, index) => (
-                    <li key={index}>
-                      <Badge color={color}>{text}</Badge>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="p-2">
-                <span className="flex w-max items-center gap-1 text-xs text-neutral-600 [&_svg]:size-4">
-                  <CalendarIcon />
-                  {dayMonthShortFormatter(new Date())}
-                </span>
-              </div>
-            </article>
-          </li>
-        ))}
-      </ul>
+    <div className="flex h-100 w-full flex-col items-center overflow-hidden px-6 md:px-8 @3xl:h-150 @3xl:items-end">
+      <GuidesCarousel />
     </div>
   )
 }
