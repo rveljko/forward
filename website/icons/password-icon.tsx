606f9d73647aa19d75a68bca22899e60832133e3
Veljko <radisavljevicveljko123@gmail.com>
2026-03-09 15:58:00 +0100

    add aria-hidden to security feature icons

--- website/icons/password-icon.tsx
+++ website/icons/password-icon.tsx
@@ -6,6 +6,7 @@ export default function PasswordIcon() {
       viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      aria-hidden
     >
       <path
         d="M10 8.3335V11.6668"
