802fbe226fcc5d8056eaa4d9ae59408cf97ca5a0
Veljko <radisavljevicveljko123@gmail.com>
2026-03-23 12:15:46 +0100

    add max width to drafts panel

--- website/components/drafts-panel.tsx
+++ website/components/drafts-panel.tsx
@@ -24,7 +24,7 @@ export default function DraftsPanel() {
       <div className="border-b border-b-black/10 p-4 text-sm font-medium">
         Drafts
       </div>
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(min(--spacing(64),100%),1fr))] gap-4 px-4 py-8">
+      <div className="mx-auto grid max-w-180 grid-cols-[repeat(auto-fit,minmax(min(--spacing(48),100%),1fr))] gap-4 px-4 py-8">
         <span className="bg-dashboard-neutral-200 flex size-full flex-col items-center justify-center gap-2 rounded-lg border border-black/10 px-4 py-8 shadow-sm">
           <div className="flex size-16 items-center justify-center rounded-full bg-black/10 [&_svg]:size-8">
             <PlusIcon />
