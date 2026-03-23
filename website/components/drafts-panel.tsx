b4448abe75bbab49c1d13e8d76f2853529b1ed6a
Veljko <radisavljevicveljko123@gmail.com>
2026-03-23 12:09:34 +0100

    add new drafts to drafts panel

--- website/components/drafts-panel.tsx
+++ website/components/drafts-panel.tsx
@@ -11,6 +11,11 @@ const drafts: Draft[] = [
   { title: 'Offline mode support for dashboard idea', daysAgo: '1d ago' },
   { title: 'Note for next meeting with John', daysAgo: '2d ago' },
   { title: 'Design feedback meeting with Luna', daysAgo: '3d ago' },
+  { title: 'User onboarding and training improvements', daysAgo: '2w ago' },
+  {
+    title: 'Performance and load testing meeting with William',
+    daysAgo: '3w ago',
+  },
 ]
 
 export default function DraftsPanel() {
