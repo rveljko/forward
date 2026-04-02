e5788db9d133cc7b62f8b681f90eb54fd8986764
Veljko <radisavljevicveljko123@gmail.com>
2026-04-02 19:12:16 +0200

    fix draft card footer

--- website/components/draft-card.tsx
+++ website/components/draft-card.tsx
@@ -10,16 +10,16 @@ export default function DraftCard({
   draft: { title, daysAgo },
 }: DraftCardProps) {
   return (
-    <article className="h-full rounded-lg shadow-sm ring ring-black/10">
+    <article className="flex h-full flex-col rounded-lg shadow-sm ring ring-black/10">
       <div className="overflow-hidden rounded-t-lg">
         <div className="aspect-3/1 w-full bg-black/10"></div>
       </div>
-      <div className="h-14 border-b border-b-black/10 p-2">
+      <div className="h-14 p-2">
         <span className="line-clamp-2 text-sm font-medium text-balance">
           {title}
         </span>
       </div>
-      <div className="flex items-center justify-between p-2">
+      <div className="mt-auto flex items-center justify-between border-t border-t-black/10 p-2">
         <span className="text-dashboard-neutral-600 flex items-center gap-1 text-xs [&_svg]:size-4">
           <ClockIcon /> {daysAgo}
         </span>
