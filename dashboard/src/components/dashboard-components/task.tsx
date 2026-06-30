4c85eb7acca45a646aa4b06ae4e78a5ab059d4dc
Veljko <radisavljevicveljko123@gmail.com>
2026-06-30 16:32:15 +0200

    fix task colors and font sizes

--- dashboard/src/components/dashboard-components/task.tsx
+++ dashboard/src/components/dashboard-components/task.tsx
@@ -44,24 +44,24 @@ export default function Task({
         e.preventDefault()
         openModal()
       }}
-      className={`border-b-section-outline hover:bg-clickable/5 relative flex items-center justify-between gap-2 border-b p-4 ${activeTaskId === id ? 'bg-clickable/5' : 'bg-section-background-color'}`}
+      className={`relative flex items-center justify-between gap-2 border-b border-b-neutral-200 p-4 hover:bg-white ${activeTaskId === id ? 'bg-white' : 'bg-neutral-50'}`}
     >
       <label className="flex items-center justify-center gap-2 hover:cursor-pointer has-checked:line-through">
         <Checkbox checked={isChecked} onChange={() => updateTaskStatus(id)} />
-        <span className="text-clickable line-clamp-1 break-all">{title}</span>
+        <span className="line-clamp-1 break-all text-black">{title}</span>
         <span className="absolute inset-0" />
       </label>
       <div className="flex items-center gap-2">
         <time
           dateTime={iso8601DateFormatter(createdAt)}
-          className="flex text-sm text-nowrap text-neutral-400"
+          className="flex text-xs text-nowrap text-neutral-600"
         >
           {dayMonthShortFormatter(createdAt)}
         </time>
         <Button
           variant="tertiary"
           onClick={openModal}
-          className="hover:text-clickable isolate -m-2 rounded-full p-2 text-neutral-400"
+          className="isolate -m-2 rounded-full p-2 text-neutral-600 hover:text-black"
         >
           <DotsVerticalIcon />
           <span className="sr-only">Actions</span>
