48e8ef1da1bc506d387b3e05e7f7bcf080c373be
Veljko <radisavljevicveljko123@gmail.com>
2026-06-30 16:12:06 +0200

    fix divider colors

--- dashboard/src/components/dashboard-components/ui/divider.tsx
+++ dashboard/src/components/dashboard-components/ui/divider.tsx
@@ -3,8 +3,8 @@ type DividerProps = React.ComponentPropsWithoutRef<'div'>
 export default function Divider({ ...props }: DividerProps) {
   return (
     <div role="separator" {...props}>
-      <div className="bg-divider-top-background h-0.25 w-full" />
-      <div className="bg-divider-bottom-background h-0.25 w-full" />
+      <div className="h-px w-full bg-neutral-200" />
+      <div className="h-px w-full bg-white" />
     </div>
   )
 }
