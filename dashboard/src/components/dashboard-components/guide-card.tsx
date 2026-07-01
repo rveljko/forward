791a915b05b3ae4392cc974ddf4fa710160526a5
Veljko <radisavljevicveljko123@gmail.com>
2026-07-01 16:18:00 +0200

    fix guide card colors

--- dashboard/src/components/dashboard-components/guide-card.tsx
+++ dashboard/src/components/dashboard-components/guide-card.tsx
@@ -1,3 +1,4 @@
+import Badge from '@dashboard-components/ui/badge'
 import Divider from '@dashboard-components/ui/divider'
 import CalendarIcon from '@icons/calendar-icon'
 import { usePreferences } from '@services/contexts/preferences-context'
@@ -13,45 +14,42 @@ type GuideCardProps = {
 }
 
 export default function GuideCard({
-  guide: { title, slug, thumbnailDarkUrl, thumbnailLightUrl, lastEdit },
+  guide: { title, slug, thumbnailDarkUrl, thumbnailLightUrl, lastEdit, tags },
 }: GuideCardProps) {
   const { isLightTheme } = usePreferences()
 
   return (
-    <article className="ring-section-outline pointer-coarse:active:ring-clickable/20 hover:ring-clickable/20 relative flex h-full flex-col overflow-hidden rounded-lg shadow-sm ring pointer-coarse:transition-all pointer-coarse:active:scale-99">
-      <div className="grow overflow-hidden rounded-t-lg">
-        <div className="from-brand-600 to-brand-500 flex w-full items-center justify-center bg-linear-0 p-4">
-          <div className="aspect-16/10 w-full">
-            <img
-              src={isLightTheme ? thumbnailLightUrl : thumbnailDarkUrl}
-              alt=""
-              className="rounded-sm"
-            />
-          </div>
-        </div>
+    <article className="relative overflow-hidden rounded-lg border border-white bg-white shadow-sm ring ring-neutral-900/10 hover:cursor-pointer hover:ring-neutral-900/20 has-[a:focus-visible]:ring-2 has-[a:focus-visible]:ring-black pointer-coarse:transition pointer-coarse:active:scale-99 pointer-coarse:active:ring-neutral-900/20">
+      <div className="max-h-42 w-full overflow-hidden mask-linear-360 mask-linear-from-transparent mask-linear-to-black">
+        <img src={isLightTheme ? thumbnailLightUrl : thumbnailDarkUrl} alt="" />
       </div>
-      <div className="mt-auto">
-        <div className="h-16 p-2">
-          <h3>
-            <Link
-              to={`/guides/${slug}`}
-              className="text-clickable line-clamp-2 text-balance"
-            >
-              <span className="absolute inset-0" />
-              {title}
-            </Link>
-          </h3>
-        </div>
-        <Divider />
-        <div className="p-2">
-          <div className="flex items-center gap-1 text-sm text-neutral-400">
-            <span className="[&_svg]:size-4">
-              <CalendarIcon />
-            </span>
-            <time dateTime={iso8601DateFormatter(lastEdit)}>
-              {shortMonthYearFormatter(lastEdit)}
-            </time>
-          </div>
+      <div className="p-2">
+        <h3>
+          <Link
+            to={`/guides/${slug}`}
+            className="mb-1 block text-balance text-black focus:outline-0"
+          >
+            <span className="absolute inset-0" />
+            {title}
+          </Link>
+        </h3>
+        <ul className="flex items-center gap-1">
+          {tags.map(({ text, color }, index) => (
+            <li key={index}>
+              <Badge color={color}>{text}</Badge>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Divider />
+      <div className="p-2">
+        <div className="flex items-center gap-1 text-xs text-neutral-600">
+          <span className="[&_svg]:size-4">
+            <CalendarIcon />
+          </span>
+          <time dateTime={iso8601DateFormatter(lastEdit)}>
+            {shortMonthYearFormatter(lastEdit)}
+          </time>
         </div>
       </div>
     </article>
