dc650915316e2a20051d5b15e0d8576c51915ecc
Veljko <radisavljevicveljko123@gmail.com>
2026-07-02 12:44:59 +0200

    fix preferences card colors and font weight

--- dashboard/src/components/dashboard-components/preferences-card.tsx
+++ dashboard/src/components/dashboard-components/preferences-card.tsx
@@ -12,11 +12,11 @@ export default function PreferencesCard({
   return (
     <article className="w-full max-w-max">
       <div
-        className={`relative mb-1 size-full max-h-27 max-w-40 overflow-hidden rounded-sm bg-neutral-800 after:pointer-events-none after:absolute after:inset-0 after:rounded-sm after:inset-ring-2 ${isActive ? 'after:inset-ring-brand-500' : 'after:inset-ring-neutral-700'}`}
+        className={`relative mb-1 size-full max-h-27 max-w-40 overflow-hidden rounded-md bg-neutral-100 after:pointer-events-none after:absolute after:inset-0 after:rounded-md after:inset-ring-2 ${isActive ? 'after:inset-ring-brand-500' : 'after:inset-ring-neutral-200'}`}
       >
         <img src={imageUrl} alt="" className="mt-2 ml-2 rounded-xs" />
       </div>
-      <h3 className="text-left">{name}</h3>
+      <h3 className="text-left font-medium">{name}</h3>
     </article>
   )
 }
