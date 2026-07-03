3fc5010fc842c1919c6cad4b0c2d5dffc3db7b20
Veljko <radisavljevicveljko123@gmail.com>
2026-07-03 13:13:04 +0200

    fix draft actions modal panel colors

--- dashboard/src/components/dashboard-components/delete-draft-panel.tsx
+++ dashboard/src/components/dashboard-components/delete-draft-panel.tsx
@@ -1,4 +1,5 @@
 import Divider from '@dashboard-components/ui/divider'
+import IconWrapper from '@dashboard-components/ui/icon-wrapper'
 import TrashIcon from '@icons/trash-icon'
 import { useDrafts } from '@services/contexts/drafts-context'
 import Button from '@ui/button'
@@ -20,13 +21,14 @@ export default function DeleteDraftPanel({
   return (
     <>
       <div className="p-4">
-        <span className="border-section-outline bg-danger-500/10 [&_svg]:text-danger-500 mb-2 flex size-9 items-center justify-center rounded-lg border">
-          <TrashIcon />
-        </span>
-        <h3 className="mb-1">
-          Confirm <strong>{title}</strong> Deletion
-        </h3>
-        <p>Are you sure you want to delete?</p>
+        <IconWrapper
+          icon={<TrashIcon />}
+          className="mb-2 bg-red-100 text-red-500 ring-red-200"
+        />
+        <h3 className="mb-1 font-medium">Confirm {title} Deletion</h3>
+        <p className="text-xs text-neutral-600">
+          Are you sure you want to delete?
+        </p>
       </div>
       <Divider />
       <div className="flex items-center justify-end gap-2 p-4">
