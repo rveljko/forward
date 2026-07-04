f05dd7d08d2712cc6585d39ccce0fea679123617
Veljko <radisavljevicveljko123@gmail.com>
2026-07-04 12:41:08 +0200

    fix tooltip and tooltip button anchor positioning

--- dashboard/src/components/dashboard-components/ui/tooltip-button.tsx
+++ dashboard/src/components/dashboard-components/ui/tooltip-button.tsx
@@ -1,5 +1,6 @@
 import Tooltip, { tooltip } from '@dashboard-components/ui/tooltip'
 import Button, { ButtonProps } from '@ui/button'
+import { cn } from '@utils/utils'
 import { VariantProps } from 'class-variance-authority'
 import { useState } from 'react'
 
@@ -11,15 +12,17 @@ type TooltipButtonProps = ButtonProps &
 export default function TooltipButton({
   position,
   tooltip: TooltipContent,
+  className,
   ...props
 }: TooltipButtonProps) {
   const [isHovered, setIsHovered] = useState(false)
 
   return (
-    <div className="[anchor-scope:--button] not-supports-[anchor-name:--dropdown]:relative">
+    <div className="anchor-scope/tooltip">
       <Button
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
+        className={cn('anchor/tooltip', className)}
         {...props}
       />
       {isHovered && <Tooltip position={position}>{TooltipContent}</Tooltip>}
--- dashboard/src/components/dashboard-components/ui/tooltip.tsx
+++ dashboard/src/components/dashboard-components/ui/tooltip.tsx
@@ -2,22 +2,16 @@ import { cn } from '@utils/utils'
 import { cva, VariantProps } from 'class-variance-authority'
 
 export const tooltip = cva(
-  'absolute z-999 my-2 rounded-sm border border-neutral-200 bg-white p-1 text-black [position-anchor:--button] [position-try-fallbacks:flip-inline,flip-block]',
+  'anchored/tooltip try-flip-all absolute z-999 my-2 rounded-sm border border-neutral-200 bg-white p-1 text-black',
   {
     variants: {
       position: {
-        'top-left':
-          '[position-area:top_span-left] not-supports-[position-area:top_span-left]:right-0 not-supports-[position-area:top_span-left]:bottom-full',
-        'top-center':
-          '[position-area:top_center] not-supports-[position-area:top_center]:bottom-full not-supports-[position-area:top_center]:left-1/2 not-supports-[position-area:top_center]:-translate-x-1/2',
-        'top-right':
-          '[position-area:top_span-right] not-supports-[position-area:top_span-right]:bottom-full not-supports-[position-area:top_span-right]:left-0',
-        'bottom-left':
-          '[position-area:bottom_span-left] not-supports-[position-area:bottom_span-left]:top-full not-supports-[position-area:bottom_span-left]:right-0',
-        'bottom-center':
-          '[position-area:bottom_center] not-supports-[position-area:bottom_center]:top-full not-supports-[position-area:bottom_center]:left-1/2 not-supports-[position-area:bottom_center]:-translate-x-1/2',
-        'bottom-right':
-          '[position-area:bottom_span-right] not-supports-[position-area:bottom_span-right]:top-full not-supports-[position-area:bottom_span-right]:left-0',
+        'top-left': 'anchored-top-span-left',
+        'top-center': 'anchored-top-center',
+        'top-right': 'anchored-top-span-right',
+        'bottom-left': 'anchored-bottom-span-left',
+        'bottom-center': 'anchored-bottom-center',
+        'bottom-right': 'anchored-bottom-span-right',
       },
     },
     defaultVariants: {
