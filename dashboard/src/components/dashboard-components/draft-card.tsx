5b0d6c030f8e054df287c6a115943f0686efcd46
Veljko <radisavljevicveljko123@gmail.com>
2026-07-04 11:54:40 +0200

    add fix height on banner in draft card

--- dashboard/src/components/dashboard-components/draft-card.tsx
+++ dashboard/src/components/dashboard-components/draft-card.tsx
@@ -33,7 +33,7 @@ export default function DraftCard({
       <div className="overflow-hidden">
         <div
           className={cn(
-            'aspect-3/1 w-full rounded-b-sm',
+            'h-21 w-full rounded-b-sm',
             category === 'general' && 'bg-neutral-200',
             category === 'idea' && 'bg-sky-200',
             category === 'meeting' && 'bg-green-200',
