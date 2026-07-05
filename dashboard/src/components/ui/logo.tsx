5cdfde1c724313ab2c52a1b812203f3c26ba7f82
Veljko <radisavljevicveljko123@gmail.com>
2026-07-05 13:14:04 +0200

    fix logo color

--- dashboard/src/components/ui/logo.tsx
+++ dashboard/src/components/ui/logo.tsx
@@ -6,7 +6,7 @@ type LogoProps = {
 
 export default function Logo({ hideText }: LogoProps) {
   return (
-    <div className="text-clickable group/logo flex items-center gap-1">
+    <div className="group/logo flex items-center gap-1 text-black">
       <span className="shrink-0">
         <LogoIcon className="[--clip-path:polygon(0_0,100%_0,100%_100%,0_100%)] group-hover/logo:[&_rect]:[clip-path:var(--clip-path)] pointer-coarse:group-active/logo:[&_rect]:[clip-path:var(--clip-path)]" />
       </span>
