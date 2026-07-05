51b67017dbf6461b93bec75fcfa54cc70a71b85a
Veljko <radisavljevicveljko123@gmail.com>
2026-07-05 13:07:50 +0200

    change link font weight in issues list item

--- dashboard/src/components/dashboard-components/issues-list-item.tsx
+++ dashboard/src/components/dashboard-components/issues-list-item.tsx
@@ -89,7 +89,7 @@ export default function IssuesListItem({
           <h3>
             <Link
               to={`/issues/${id}`}
-              className="line-clamp-1 break-all text-black"
+              className="line-clamp-1 font-medium break-all text-black"
             >
               <span className="absolute inset-0" />
               {title}
@@ -105,7 +105,7 @@ export default function IssuesListItem({
         <Button
           variant="tertiary"
           onClick={openModal}
-          className="hover:text-clickable isolate -m-2 rounded-full p-2 text-neutral-600"
+          className="isolate -m-2 rounded-full p-2 text-neutral-600 hover:text-black"
         >
           <DotsVerticalIcon />
           <span className="sr-only">Actions</span>
