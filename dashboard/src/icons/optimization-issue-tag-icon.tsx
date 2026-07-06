de5e540a59b2eb6f98988e32f7313b1a42e66817
Veljko <radisavljevicveljko123@gmail.com>
2026-07-06 13:11:58 +0200

    change optimization issue icon color

--- dashboard/src/icons/optimization-issue-tag-icon.tsx
+++ dashboard/src/icons/optimization-issue-tag-icon.tsx
@@ -1,7 +1,7 @@
 export default function OptimizationIssueTagIcon() {
   return (
     <div className="flex size-5 items-center justify-center">
-      <div className="size-2 rounded-full bg-yellow-500" />
+      <div className="size-2 rounded-full bg-green-500" />
     </div>
   )
 }
