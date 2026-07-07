4e754626a0162b1ddd09ce6e8ccf41c27072e079
Veljko <radisavljevicveljko123@gmail.com>
2026-07-07 12:40:03 +0200

    animate issues kanban cards list

--- dashboard/src/components/dashboard-components/issues-kanban-cards-list.tsx
+++ dashboard/src/components/dashboard-components/issues-kanban-cards-list.tsx
@@ -1,7 +1,9 @@
 import IssuesKanbanCard from '@dashboard-components/issues-kanban-card'
+import { useDndContext } from '@dnd-kit/core'
 import { SortableContext } from '@dnd-kit/sortable'
 import { Issue } from '@utils/types'
 import { cn } from '@utils/utils'
+import { AnimatePresence, motion } from 'motion/react'
 
 type IssuesKanbanCardsListProps = React.ComponentPropsWithRef<'ul'> & {
   issues: Issue[]
@@ -12,14 +14,26 @@ export default function IssuesKanbanCardsList({
   className,
   ...props
 }: IssuesKanbanCardsListProps) {
+  const { active } = useDndContext()
+
+  const isDragging = active !== null
+
   return (
     <ul className={cn('flex grow flex-col gap-2', className)} {...props}>
       <SortableContext items={issues}>
-        {issues.map((issue) => (
-          <li key={issue.id}>
-            <IssuesKanbanCard issue={issue} />
-          </li>
-        ))}
+        <AnimatePresence mode="popLayout" initial={false}>
+          {issues.map((issue) => (
+            <motion.li
+              layout={!isDragging}
+              key={issue.id}
+              initial={{ opacity: 0, scale: 0.95 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.95 }}
+            >
+              <IssuesKanbanCard issue={issue} />
+            </motion.li>
+          ))}
+        </AnimatePresence>
       </SortableContext>
     </ul>
   )
