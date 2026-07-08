462fe122fbd6a6d7e6690d9a6a688675f7a9766f
Veljko <radisavljevicveljko123@gmail.com>
2026-07-08 10:40:40 +0200

    fix issues list board

--- dashboard/src/components/dashboard-components/issues-list-board.tsx
+++ dashboard/src/components/dashboard-components/issues-list-board.tsx
@@ -5,24 +5,21 @@ import { DragOverlay } from '@dnd-kit/core'
 import { useIssues } from '@services/contexts/issues-context'
 import { Issue } from '@utils/types'
 
-type IssuesListBoardProps = React.ComponentPropsWithoutRef<'div'> & {
+type IssuesListBoardProps = {
   activeId: Issue['id'] | null
 }
 
-export default function IssuesListBoard({
-  activeId,
-  ...props
-}: IssuesListBoardProps) {
+export default function IssuesListBoard({ activeId }: IssuesListBoardProps) {
   const { getIssueById } = useIssues()
 
   return (
-    <div className="overflow-y-auto" {...props}>
+    <>
       {issueStatuses.map(({ id, label }) => (
         <IssuesListColumn status={label} key={id} />
       ))}
       <DragOverlay>
         {activeId && <IssuesListItem issue={getIssueById(activeId)} />}
       </DragOverlay>
-    </div>
+    </>
   )
 }
