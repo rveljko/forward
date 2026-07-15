460b0a1ce7e68519611fda1949c3f9771ec5384f
Veljko <radisavljevicveljko123@gmail.com>
2026-07-15 16:27:47 +0200

    add search filter focus visible state stylings

--- dashboard/src/components/dashboard-components/ui/search-filter.tsx
+++ dashboard/src/components/dashboard-components/ui/search-filter.tsx
@@ -16,7 +16,7 @@ export default function SearchFilter({
   return (
     <label
       className={cn(
-        'flex w-max items-center gap-1 rounded-md px-2 py-1 text-xs shadow-sm ring inset-ring ring-neutral-900/10 inset-ring-white transition hover:cursor-pointer hover:ring-neutral-900/20 active:scale-99',
+        'flex w-max items-center gap-1 rounded-md px-2 py-1 text-xs shadow-sm ring inset-ring ring-neutral-900/10 inset-ring-white transition hover:cursor-pointer hover:ring-neutral-900/20 active:scale-99 has-[input[type=checkbox]:focus-visible]:ring-2 has-[input[type=checkbox]:focus-visible]:ring-black has-[input[type=checkbox]:focus-visible]:transition-none',
         checked &&
           'ring-brand-600 bg-brand-500 pointer-coarse:active:ring-brand-700 pointer-coarse:active:bg-brand-600 hover:ring-brand-700 hover:bg-brand-600 text-white inset-ring-white/10',
         className
