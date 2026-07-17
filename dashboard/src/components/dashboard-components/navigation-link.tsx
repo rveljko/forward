766800cc892ea8735446a33034af43ea900c9a26
Veljko <radisavljevicveljko123@gmail.com>
2026-07-17 11:25:27 +0200

    remove span icon wrappers from navigation link

--- dashboard/src/components/dashboard-components/navigation-link.tsx
+++ dashboard/src/components/dashboard-components/navigation-link.tsx
@@ -18,23 +18,18 @@ export default function NavigationLink({
     <NavLink
       className={({ isActive }) =>
         cn(
-          `group flex items-center gap-1 rounded-md px-1.5 py-1 font-medium text-black transition hover:bg-neutral-200 active:scale-99 pointer-coarse:active:bg-neutral-200 ${isActive ? 'bg-neutral-50 ring inset-ring ring-neutral-900/10 inset-ring-white hover:bg-neutral-50 hover:ring-neutral-900/20 pointer-coarse:active:bg-neutral-50 pointer-coarse:active:ring-neutral-900/20 [&>span:first-child]:text-black' : ''}`,
+          'flex items-center gap-1 rounded-md px-1.5 py-1 font-medium text-black transition hover:bg-neutral-200 active:scale-99 pointer-coarse:active:bg-neutral-200',
+          '[&_svg]:size-4 [&_svg]:text-neutral-600 hover:[&_svg]:text-black pointer-coarse:active:[&_svg]:text-black',
+          isActive &&
+            'bg-neutral-50 ring inset-ring ring-neutral-900/10 inset-ring-white hover:bg-neutral-50 hover:ring-neutral-900/20 pointer-coarse:active:bg-neutral-50 pointer-coarse:active:ring-neutral-900/20 [&>svg:first-child]:text-black',
           className
         )
       }
       {...props}
     >
-      {LeftIcon && (
-        <span className="text-neutral-600 transition-colors group-hover:text-black pointer-coarse:group-active:text-black [&_svg]:size-4">
-          {LeftIcon}
-        </span>
-      )}
+      {LeftIcon}
       {children}
-      {RightIcon && (
-        <span className="text-neutral-600 transition-colors group-hover:text-black pointer-coarse:group-active:text-black [&_svg]:size-4">
-          {RightIcon}
-        </span>
-      )}
+      {RightIcon}
     </NavLink>
   )
 }
