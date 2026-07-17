96bbc5a5d3fe866d0249890e88efaf90abe83fc7
Veljko <radisavljevicveljko123@gmail.com>
2026-07-17 18:34:15 +0200

    fix checkbox shrinking

--- dashboard/src/components/dashboard-components/ui/checkbox.tsx
+++ dashboard/src/components/dashboard-components/ui/checkbox.tsx
@@ -7,7 +7,7 @@ export default function Checkbox({ className, ...props }: CheckboxProps) {
     <input
       type="checkbox"
       className={cn(
-        'relative size-3.5 appearance-none',
+        'relative size-3.5 shrink-0 appearance-none',
         'hover:before:inset-ring-brand-500 pointer-coarse:active:before:inset-ring-brand-500 before:absolute before:flex before:size-full before:items-center before:justify-center before:rounded-sm before:bg-white before:text-xs before:text-transparent before:inset-ring before:inset-ring-neutral-300 before:transition-all',
         "checked:before:bg-brand-500 checked:before:text-white checked:before:inset-ring-0 checked:before:content-['✓']",
         className
