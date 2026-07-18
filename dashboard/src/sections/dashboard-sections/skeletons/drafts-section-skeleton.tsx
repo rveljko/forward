1da7195a6fcbaab430d7cb5d024a68ce2f77ac12
Veljko <radisavljevicveljko123@gmail.com>
2026-07-18 13:47:18 +0200

    fix drafts section skeleton

--- dashboard/src/sections/dashboard-sections/skeletons/drafts-section-skeleton.tsx
+++ dashboard/src/sections/dashboard-sections/skeletons/drafts-section-skeleton.tsx
@@ -1,25 +1,33 @@
 import Container from '@dashboard-components/container'
+import Divider from '@dashboard-components/ui/divider'
 import Skeleton from '@dashboard-components/ui/skeleton'
-import { drafts } from '@data/drafts'
 
 export default function DraftsSectionSkeleton() {
   return (
-    <section>
+    <section className="flex h-full flex-col overflow-hidden">
       <div className="p-4">
         <Skeleton className="max-w-40" />
       </div>
-      <Skeleton className="h-0.5 rounded-none" />
-      <Container className="py-8 md:py-16">
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(min(var(--draft-card-width),100%),1fr))] gap-4">
-          <Skeleton className="h-full min-h-40.5" />
-          {drafts.map((_, index) => (
-            <Skeleton key={index} className="h-auto">
-              <div className="aspect-3/1 w-full" />
-              <div className="h-25.5" />
-            </Skeleton>
-          ))}
+      <Divider />
+      <div className="flex flex-wrap items-center justify-between gap-2 p-4">
+        <div className="flex items-center gap-2">
+          <Skeleton className="h-7 w-18 shrink-0" />
+          <Skeleton className="h-7 w-18 shrink-0" />
         </div>
-      </Container>
+        <Skeleton className="h-7.5 w-40" />
+      </div>
+      <Divider />
+      <div className="grow scrollbar-gutter-stable overflow-y-auto">
+        <Container className="py-8 md:py-16">
+          <div className="grid grid-cols-[repeat(auto-fit,minmax(min(var(--draft-card-width),100%),1fr))] gap-4">
+            <Skeleton className="h-43.5" />
+            <Skeleton className="h-43.5" />
+            <Skeleton className="h-43.5" />
+            <Skeleton className="h-43.5" />
+            <Skeleton className="h-43.5" />
+          </div>
+        </Container>
+      </div>
     </section>
   )
 }
