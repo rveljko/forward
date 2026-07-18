ddeaf17bf633164cbd2a85d4dcf591b282b8ffc3
Veljko <radisavljevicveljko123@gmail.com>
2026-07-18 13:26:27 +0200

    fix issues section skeleton

--- dashboard/src/sections/dashboard-sections/skeletons/issues-section-skeleton.tsx
+++ dashboard/src/sections/dashboard-sections/skeletons/issues-section-skeleton.tsx
@@ -1,3 +1,4 @@
+import Divider from '@dashboard-components/ui/divider'
 import Skeleton from '@dashboard-components/ui/skeleton'
 
 function getView(): 'list' | 'kanban' {
@@ -7,20 +8,20 @@ function getView(): 'list' | 'kanban' {
 
 export default function IssuesSectionSkeleton() {
   return (
-    <section className="flex h-full flex-col">
+    <section className="flex h-full flex-col overflow-y-hidden">
       <div className="flex items-center justify-between gap-2 p-4">
         <Skeleton className="max-w-40" />
         <Skeleton className="aspect-square w-auto" />
       </div>
-      <Skeleton className="min-h-0.5 rounded-none" />
+      <Divider />
       <div className="flex flex-wrap items-center justify-between gap-2 p-4">
         <div className="flex items-center gap-2">
-          <Skeleton className="h-8 w-20 shrink-0" />
-          <Skeleton className="h-8 w-20 shrink-0" />
+          <Skeleton className="h-7 w-18 shrink-0" />
+          <Skeleton className="h-7 w-18 shrink-0" />
         </div>
-        <Skeleton className="h-8 w-25" />
+        <Skeleton className="h-7.5 w-35" />
       </div>
-      <Skeleton className="min-h-0.5 rounded-none" />
+      <Divider />
       {getView() === 'list' && <ListViewSkeleton />}
       {getView() === 'kanban' && <KanbanViewSkeleton />}
     </section>
@@ -29,59 +30,22 @@ export default function IssuesSectionSkeleton() {
 
 function ListViewSkeleton() {
   return (
-    <>
-      <div>
-        <Skeleton className="h-14 rounded-none" />
+    <div className="overflow-x-hidden overflow-y-auto">
+      <div className="flex flex-col">
+        <div className="sticky top-0 z-10">
+          <Skeleton className="h-13.25 animate-none rounded-none" />
+        </div>
         <div className="space-y-px">
-          <div className="flex items-center justify-between gap-2 p-4">
-            <Skeleton className="max-w-60" />
-            <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
-          </div>
-          <div className="flex items-center justify-between gap-2 p-4">
-            <Skeleton className="max-w-60" />
-            <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
-          </div>
-          <div className="flex items-center justify-between gap-2 p-4">
-            <Skeleton className="max-w-60" />
-            <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
-          </div>
           <div className="flex items-center justify-between gap-2 p-4">
             <Skeleton className="max-w-60" />
             <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
           </div>
         </div>
       </div>
-      <div>
-        <Skeleton className="h-14 rounded-none" />
-        <div className="space-y-px">
-          <div className="flex items-center justify-between gap-2 p-4">
-            <Skeleton className="max-w-60" />
-            <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
-          </div>
-          <div className="flex items-center justify-between gap-2 p-4">
-            <Skeleton className="max-w-60" />
-            <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
-          </div>
-          <div className="flex items-center justify-between gap-2 p-4">
-            <Skeleton className="max-w-60" />
-            <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
-          </div>
-          <div className="flex items-center justify-between gap-2 p-4">
-            <Skeleton className="max-w-60" />
-            <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
-          </div>
-          <div className="flex items-center justify-between gap-2 p-4">
-            <Skeleton className="max-w-60" />
-            <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
-          </div>
-          <div className="flex items-center justify-between gap-2 p-4">
-            <Skeleton className="max-w-60" />
-            <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
-          </div>
+      <div className="flex flex-col">
+        <div className="sticky top-0 z-10">
+          <Skeleton className="h-13.25 animate-none rounded-none" />
         </div>
-      </div>
-      <div>
-        <Skeleton className="h-14 rounded-none" />
         <div className="space-y-px">
           <div className="flex items-center justify-between gap-2 p-4">
             <Skeleton className="max-w-60" />
@@ -97,9 +61,15 @@ function ListViewSkeleton() {
           </div>
         </div>
       </div>
-      <div>
-        <Skeleton className="h-14 rounded-none" />
+      <div className="flex flex-col">
+        <div className="sticky top-0 z-10">
+          <Skeleton className="h-13.25 animate-none rounded-none" />
+        </div>
         <div className="space-y-px">
+          <div className="flex items-center justify-between gap-2 p-4">
+            <Skeleton className="max-w-60" />
+            <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
+          </div>
           <div className="flex items-center justify-between gap-2 p-4">
             <Skeleton className="max-w-60" />
             <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
@@ -110,173 +80,84 @@ function ListViewSkeleton() {
           </div>
         </div>
       </div>
-      <div>
-        <Skeleton className="h-14 rounded-none" />
+      <div className="flex flex-col">
+        <div className="sticky top-0 z-10">
+          <Skeleton className="h-13.25 animate-none rounded-none" />
+        </div>
         <div className="space-y-px">
           <div className="flex items-center justify-between gap-2 p-4">
             <Skeleton className="max-w-60" />
             <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
           </div>
+          <div className="flex items-center justify-between gap-2 p-4">
+            <Skeleton className="max-w-60" />
+            <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="sticky top-0 z-10">
+            <Skeleton className="h-13.25 animate-none rounded-none" />
+          </div>
+          <div className="space-y-px">
+            <div className="flex items-center justify-between gap-2 p-4">
+              <Skeleton className="max-w-60" />
+              <Skeleton className="max-md:aspect-square max-md:w-auto md:h-7.5 md:max-w-60" />
+            </div>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
 function KanbanViewSkeleton() {
   return (
-    <div className="w-0 min-w-full grow">
-      <div className="flex h-full gap-4.25 overflow-x-auto p-4">
-        <div className="flex h-full w-85 shrink-0 flex-col gap-2">
-          <Skeleton className="h-14" />
-          <div className="flex grow flex-col gap-2">
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
+    <div className="min-h-0 flex-1 overflow-y-auto">
+      <div className="h-full w-0 min-w-full overflow-x-auto">
+        <div className="flex min-h-full items-stretch gap-px py-4">
+          <div className="pr-2 pl-4">
+            <div className="flex min-h-full w-85 shrink-0 flex-col gap-2">
+              <Skeleton className="sticky top-0 z-10 h-13 animate-none" />
+              <div className="flex min-h-0 flex-1 flex-col gap-2">
+                <Skeleton className="h-29" />
               </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
-            </div>
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
-              </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
-            </div>
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
-              </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
-            </div>
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
-              </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
             </div>
           </div>
-        </div>
-        <div className="flex h-full w-85 shrink-0 flex-col gap-2">
-          <Skeleton className="h-14" />
-          <div className="flex grow flex-col gap-2">
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
+          <div className="px-2">
+            <div className="flex min-h-full w-85 shrink-0 flex-col gap-2">
+              <Skeleton className="sticky top-0 z-10 h-13 animate-none" />
+              <div className="flex min-h-0 flex-1 flex-col gap-2">
+                <Skeleton className="h-29" />
+                <Skeleton className="h-29" />
+                <Skeleton className="h-29" />
               </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
-            </div>
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
-              </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
-            </div>
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
-              </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
-            </div>
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
-              </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
-            </div>
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
-              </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
-            </div>
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
-              </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
             </div>
           </div>
-        </div>
-        <div className="flex h-full w-85 shrink-0 flex-col gap-2">
-          <Skeleton className="h-14" />
-          <div className="flex grow flex-col gap-2">
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
+          <div className="px-2">
+            <div className="flex min-h-full w-85 shrink-0 flex-col gap-2">
+              <Skeleton className="sticky top-0 z-10 h-13 animate-none" />
+              <div className="flex min-h-0 flex-1 flex-col gap-2">
+                <Skeleton className="h-29" />
+                <Skeleton className="h-29" />
+                <Skeleton className="h-29" />
               </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
-            </div>
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
-              </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
-            </div>
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
-              </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
             </div>
           </div>
-        </div>
-        <div className="flex h-full w-85 shrink-0 flex-col gap-2">
-          <Skeleton className="h-14" />
-          <div className="flex grow flex-col gap-2">
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
+          <div className="px-2">
+            <div className="flex min-h-full w-85 shrink-0 flex-col gap-2">
+              <Skeleton className="sticky top-0 z-10 h-13 animate-none" />
+              <div className="flex min-h-0 flex-1 flex-col gap-2">
+                <Skeleton className="h-29" />
+                <Skeleton className="h-29" />
               </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
-            </div>
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
-              </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
             </div>
           </div>
-        </div>
-        <div className="flex h-full w-85 shrink-0 flex-col gap-2">
-          <Skeleton className="h-14" />
-          <div className="flex grow flex-col gap-2">
-            <div className="p-4">
-              <div className="mb-4 flex justify-between gap-2">
-                <Skeleton className="max-w-60" />
-                <Skeleton className="aspect-square w-auto" />
+          <div className="pr-4 pl-2">
+            <div className="flex min-h-full w-85 shrink-0 flex-col gap-2">
+              <Skeleton className="sticky top-0 z-10 h-13 animate-none" />
+              <div className="flex min-h-0 flex-1 flex-col gap-2">
+                <Skeleton className="h-29" />
               </div>
-              <Skeleton className="mb-2 h-5 max-w-15" />
-              <Skeleton className="h-7.5 max-w-35" />
             </div>
           </div>
         </div>
