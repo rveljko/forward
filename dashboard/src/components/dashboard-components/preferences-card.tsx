f0e2dd47d37fae784784c78d5425e4e5c4fd3cf0
Veljko <radisavljevicveljko123@gmail.com>
2026-07-19 18:07:26 +0200

    add border and ring to preferences card image

--- dashboard/src/components/dashboard-components/preferences-card.tsx
+++ dashboard/src/components/dashboard-components/preferences-card.tsx
@@ -14,7 +14,11 @@ export default function PreferencesCard({
       <div
         className={`relative mb-1 size-full max-h-27 max-w-40 overflow-hidden rounded-md bg-white after:pointer-events-none after:absolute after:inset-0 after:rounded-md after:inset-ring-2 ${isActive ? 'after:inset-ring-brand-500' : 'after:inset-ring-neutral-200'}`}
       >
-        <img src={imageUrl} alt="" className="mt-2 ml-2 rounded-xs" />
+        <img
+          src={imageUrl}
+          alt=""
+          className="mt-2 ml-2 rounded-xs border-t border-l border-white ring ring-neutral-200"
+        />
       </div>
       <h3 className="text-left font-medium">{name}</h3>
     </article>
