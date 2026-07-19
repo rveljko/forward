3f320a79309b6f78c24f8832ccb6d890520ac8a1
Veljko <radisavljevicveljko123@gmail.com>
2026-07-19 14:00:24 +0200

    fix security section skeleton

--- dashboard/src/sections/dashboard-sections/skeletons/security-section-skeleton.tsx
+++ dashboard/src/sections/dashboard-sections/skeletons/security-section-skeleton.tsx
@@ -1,3 +1,4 @@
+import Divider from '@dashboard-components/ui/divider'
 import Skeleton from '@dashboard-components/ui/skeleton'
 
 export default function SecuritySectionSkeleton() {
@@ -7,22 +8,22 @@ export default function SecuritySectionSkeleton() {
         <Skeleton className="mb-1 max-w-40" />
         <Skeleton className="max-w-60" />
       </div>
-      <Skeleton className="h-0.5 rounded-none" />
+      <Divider />
       <div className="flex flex-col gap-2 lg:flex-row lg:gap-16">
         <div className="w-full max-w-50">
           <Skeleton className="max-w-30" />
         </div>
-        <Skeleton className="h-10 max-w-50 shrink-0" />
+        <Skeleton className="h-9 max-w-50 shrink-0" />
       </div>
-      <Skeleton className="h-0.5 rounded-none" />
+      <Divider />
       <div className="flex flex-col items-start gap-2 lg:flex-row lg:gap-16">
         <div className="w-full max-w-50">
           <Skeleton className="max-w-30" />
         </div>
         <Skeleton className="aspect-2/1 h-auto w-(--switch-width) rounded-full" />
       </div>
-      <Skeleton className="h-0.5 rounded-none" />
-      <Skeleton className="h-10 max-w-35" />
+      <Divider />
+      <Skeleton className="h-9.5 max-w-35" />
     </section>
   )
 }
