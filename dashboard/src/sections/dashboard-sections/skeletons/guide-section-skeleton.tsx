3e5d488c1e0ad17caa610d2a7f44d446c9dcfe92
Veljko <radisavljevicveljko123@gmail.com>
2026-07-19 12:32:20 +0200

    fix guide section skeleton

--- dashboard/src/sections/dashboard-sections/skeletons/guide-section-skeleton.tsx
+++ dashboard/src/sections/dashboard-sections/skeletons/guide-section-skeleton.tsx
@@ -1,37 +1,37 @@
 import Container from '@dashboard-components/container'
+import Divider from '@dashboard-components/ui/divider'
 import Skeleton from '@dashboard-components/ui/skeleton'
 
 export default function GuideSectionSkeleton() {
   return (
-    <section>
+    <section className="flex h-full flex-col overflow-y-hidden">
       <div className="p-4">
         <Skeleton className="max-w-90" />
       </div>
-      <Skeleton className="h-0.5 rounded-none" />
-      <Container className="py-8 md:py-16">
-        <Skeleton className="mb-2 h-8 max-w-100" />
-        <Skeleton className="mb-1 h-4.5" />
-        <Skeleton className="mb-8 h-4.5 w-2/3" />
-        <Skeleton className="mb-2 h-7 max-w-90" />
-        <Skeleton className="mb-1 h-4.5 max-w-40" />
-        <Skeleton className="mb-2 h-4.5 max-w-60" />
-        <Skeleton className="mb-1 h-4.5 max-w-50" />
-        <Skeleton className="mb-2 h-4.5 max-w-80" />
-        <Skeleton className="mb-1 h-4.5 max-w-30" />
-        <Skeleton className="mb-1 h-4.5 max-w-75" />
-        <Skeleton className="mb-1 h-4.5 max-w-75" />
-        <Skeleton className="mb-1 h-4.5 max-w-75" />
-        <Skeleton className="mb-1 h-4.5" />
-        <Skeleton className="mb-8 h-4.5 w-3/4" />
-        <Skeleton className="mb-2 h-7 max-w-90" />
-        <Skeleton className="mb-1 h-4.5 max-w-40" />
-        <Skeleton className="mb-2 h-4.5 max-w-60" />
-        <Skeleton className="mb-1 h-4.5 max-w-50" />
-        <Skeleton className="mb-1 h-4.5" />
-        <Skeleton className="mb-4 h-4.5 w-1/2" />
-        <Skeleton className="mb-1 h-4.5" />
-        <Skeleton className="mb-4 h-4.5 w-5/6" />
-      </Container>
+      <Divider />
+      <div className="overflow-y-auto py-8 md:py-16">
+        <Container>
+          <Skeleton className="mb-8 h-8 max-w-100 md:h-9" />
+          <Skeleton className="mb-1 h-4.5" />
+          <Skeleton className="mb-8 h-4.5 w-3/4" />
+          <Skeleton className="mb-1 h-7 max-w-50" />
+          <Skeleton className="mb-1 h-4.5" />
+          <Skeleton className="mb-2 h-4.5 w-2/3" />
+          <Skeleton className="mb-1 h-4.5" />
+          <Skeleton className="mb-1 h-4.5" />
+          <Skeleton className="mb-8 h-4.5 w-5/6" />
+          <Skeleton className="mb-1 h-7 max-w-60" />
+          <Skeleton className="mb-1 h-4.5" />
+          <Skeleton className="mb-1 h-4.5" />
+          <Skeleton className="mb-8 h-4.5 w-5/8" />
+          <Skeleton className="mb-1 h-7 max-w-40" />
+          <Skeleton className="mb-1 h-4.5" />
+          <Skeleton className="mb-8 h-4.5 w-2/3" />
+          <Skeleton className="mb-1 h-7 max-w-50" />
+          <Skeleton className="mb-1 h-4.5" />
+          <Skeleton className="mb-1 h-4.5" />
+        </Container>
+      </div>
     </section>
   )
 }
