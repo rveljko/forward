cb6c25f96eeb0ad99ae8e7dfeae7224206edc10a
Veljko <radisavljevicveljko123@gmail.com>
2026-07-19 18:06:58 +0200

    change preferences card background color

--- dashboard/src/components/dashboard-components/preferences-card.tsx
+++ dashboard/src/components/dashboard-components/preferences-card.tsx
@@ -12,7 +12,7 @@ export default function PreferencesCard({
   return (
     <article className="w-full max-w-max">
       <div
-        className={`relative mb-1 size-full max-h-27 max-w-40 overflow-hidden rounded-md bg-neutral-100 after:pointer-events-none after:absolute after:inset-0 after:rounded-md after:inset-ring-2 ${isActive ? 'after:inset-ring-brand-500' : 'after:inset-ring-neutral-200'}`}
+        className={`relative mb-1 size-full max-h-27 max-w-40 overflow-hidden rounded-md bg-white after:pointer-events-none after:absolute after:inset-0 after:rounded-md after:inset-ring-2 ${isActive ? 'after:inset-ring-brand-500' : 'after:inset-ring-neutral-200'}`}
       >
         <img src={imageUrl} alt="" className="mt-2 ml-2 rounded-xs" />
       </div>
