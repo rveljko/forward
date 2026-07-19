75ef4daa52dee28be408a31fc928483b53d1cc86
Veljko <radisavljevicveljko123@gmail.com>
2026-07-19 16:23:52 +0200

    change actions button sizes in issues kanban card and list item

--- dashboard/src/components/dashboard-components/issues-kanban-card.tsx
+++ dashboard/src/components/dashboard-components/issues-kanban-card.tsx
@@ -101,7 +101,7 @@ export default function IssuesKanbanCard({
           <Button
             variant="tertiary"
             onClick={openModal}
-            className="isolate -m-2 rounded-full p-2 text-neutral-600 hover:text-black"
+            className="isolate -m-1 rounded-full p-1 text-neutral-600 hover:text-black"
           >
             <DotsVerticalIcon />
             <span className="sr-only">Actions</span>
