93e4a390d23d49b00bf647bd8ada269109687cc4
Veljko <radisavljevicveljko123@gmail.com>
2026-07-22 15:06:26 +0200

    change button font weight

--- website/components/ui/button.tsx
+++ website/components/ui/button.tsx
@@ -3,7 +3,7 @@ import { cva, VariantProps } from 'class-variance-authority'
 import Link from 'next/link'
 
 const button = cva(
-  'flex w-max items-center justify-center gap-1 rounded-md text-black shadow-sm transition hover:cursor-pointer active:scale-99 pointer-coarse:select-none [&_svg]:size-4',
+  'flex w-max items-center justify-center gap-1 rounded-md font-medium text-black shadow-sm transition hover:cursor-pointer active:scale-99 pointer-coarse:select-none [&_svg]:size-4',
   {
     variants: {
       variant: {
