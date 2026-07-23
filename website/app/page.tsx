1dac870375e18a30668b2727232745bacf9ba135
Veljko <radisavljevicveljko123@gmail.com>
2026-07-23 12:29:29 +0200

    delete drafts section

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,6 +1,5 @@
 import CtaSection from '@/sections/cta-section'
 import CustomersCarouselSection from '@/sections/customers-carousel-section'
-import DraftsSection from '@/sections/drafts-section'
 import GuidesSection from '@/sections/guides-section'
 import HeroSection from '@/sections/hero-section'
 import SecuritySection from '@/sections/security-section'
@@ -15,7 +14,6 @@ export default function Home() {
       <WorkflowSection />
       <TypesSection />
       <GuidesSection />
-      <DraftsSection />
       <SecuritySection />
       <CtaSection />
     </>
