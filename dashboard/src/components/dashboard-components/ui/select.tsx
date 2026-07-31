48ab143cdbce5716d9832f7f7ea8f7415eb2b693
Veljko <radisavljevicveljko123@gmail.com>
2026-07-31 12:29:22 +0200

    change font weight in select option

--- dashboard/src/components/dashboard-components/ui/select.tsx
+++ dashboard/src/components/dashboard-components/ui/select.tsx
@@ -2,7 +2,7 @@ import { cn } from '@utils/utils'
 import { cva, VariantProps } from 'class-variance-authority'
 
 const select = cva(
-  'not-open:picker-select:opacity-(--opacity-from) not-open:picker-select:translate-y-(--slide-y-from) not-open:picker-select:scale-(--scale-from) picker-select:transition-all picker-select:transition-discrete picker-select:opacity-(--opacity-to) picker-select:translate-y-(--slide-y-to) picker-select:scale-(--scale-to) picker-select:starting:opacity-(--opacity-from) picker-select:starting:scale-(--scale-from) picker-select:starting:translate-y-(--slide-y-from) picker-select:bg-white picker-select:my-2 picker-select:border-neutral-200 picker-select:rounded-sm picker-select:p-1 picker-select:[appearance:base-select] field-sizing-content [appearance:base-select] space-y-0.5 rounded-md text-black [--opacity-from:0%] [--opacity-to:100%] [--scale-from:95%] [--scale-to:100%] [--slide-y-from:--spacing(4)] [--slide-y-to:--spacing(0)] hover:cursor-pointer active:scale-99 [&::picker-icon]:hidden',
+  'not-open:picker-select:opacity-(--opacity-from) not-open:picker-select:translate-y-(--slide-y-from) not-open:picker-select:scale-(--scale-from) picker-select:transition-all picker-select:transition-discrete picker-select:opacity-(--opacity-to) picker-select:translate-y-(--slide-y-to) picker-select:scale-(--scale-to) picker-select:starting:opacity-(--opacity-from) picker-select:starting:scale-(--scale-from) picker-select:starting:translate-y-(--slide-y-from) picker-select:bg-white picker-select:my-2 picker-select:border-neutral-200 picker-select:rounded-sm picker-select:p-1 picker-select:[appearance:base-select] field-sizing-content [appearance:base-select] space-y-0.5 rounded-md font-medium text-black [--opacity-from:0%] [--opacity-to:100%] [--scale-from:95%] [--scale-to:100%] [--slide-y-from:--spacing(4)] [--slide-y-to:--spacing(0)] hover:cursor-pointer active:scale-99 [&::picker-icon]:hidden',
   {
     variants: {
       variant: {
@@ -36,7 +36,7 @@ export default function Select({
     <select className={cn(select({ variant, size }), className)} {...props}>
       <button>
         {/* @ts-ignore */}
-        <selectedcontent className="flex items-center gap-1 font-medium"></selectedcontent>
+        <selectedcontent className="flex items-center gap-1"></selectedcontent>
       </button>
       {children}
     </select>
