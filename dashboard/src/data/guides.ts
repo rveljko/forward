6493cf48473407404311146738f64c800be7f559
Veljko <radisavljevicveljko123@gmail.com>
2026-08-04 15:07:28 +0200

    delete unnecessary guides

--- dashboard/src/data/guides.ts
+++ dashboard/src/data/guides.ts
@@ -1,11 +1,8 @@
-import { howToChangeIssuePrioritiesGuide } from '@data/guides/how-to-change-issue-priorities-guide'
 import { howToChangeYourPasswordGuide } from '@data/guides/how-to-change-your-password-guide'
 import { howToChangeYourPreferencesGuide } from '@data/guides/how-to-change-your-preferences-guide'
 import { howToCreateANewDraftGuide } from '@data/guides/how-to-create-a-new-draft-guide'
 import { howToCreateANewIssueGuide } from '@data/guides/how-to-create-a-new-issue-guide'
-import { howToSortAndFilterIssuesGuide } from '@data/guides/how-to-sort-and-filter-issues-guide'
 import { howToToggleIntegrationsGuide } from '@data/guides/how-to-toggle-integrations-guide'
-import { howToUpdateDraftPropertiesGuide } from '@data/guides/how-to-update-draft-properties-guide'
 import { howToUpdateUserInformationGuide } from '@data/guides/how-to-update-user-information-guide'
 import { IMAGE_PATH } from '@utils/constants'
 import { generateDateInPast } from '@utils/date-generators'
@@ -24,39 +21,12 @@ export const guides: Guide[] = [
   },
   {
     id: uuidv4(),
-    title: 'How to Change Issue Priorities',
-    slug: 'how-to-change-issue-priorities',
-    thumbnailUrl: `${IMAGE_PATH}guide-thumbnail-light-how-to-change-issue-priorities.png`,
-    lastEdit: generateDateInPast(1),
-    tags: [{ text: 'Issues', color: 'cyan' }],
-    content: howToChangeIssuePrioritiesGuide,
-  },
-  {
-    id: uuidv4(),
-    title: 'How to Sort and Filter Issues',
-    slug: 'how-to-sort-and-filter-issues',
-    thumbnailUrl: `${IMAGE_PATH}guide-thumbnail-light-how-to-sort-and-filter-issues.png`,
-    lastEdit: generateDateInPast(2),
-    tags: [{ text: 'Issues', color: 'cyan' }],
-    content: howToSortAndFilterIssuesGuide,
-  },
-  {
-    id: uuidv4(),
     title: 'How to Create a New Draft',
     slug: 'how-to-create-a-new-draft',
     thumbnailUrl: `${IMAGE_PATH}guide-thumbnail-light-how-to-create-a-new-draft.png`,
     lastEdit: generateDateInPast(3),
     tags: [{ text: 'Drafts', color: 'violet' }],
     content: howToCreateANewDraftGuide,
-  },
-  {
-    id: uuidv4(),
-    title: 'How to Update Draft Properties',
-    slug: 'how-to-update-draft-properties',
-    thumbnailUrl: `${IMAGE_PATH}guide-thumbnail-light-how-to-update-draft-properties.png`,
-    lastEdit: generateDateInPast(3),
-    tags: [{ text: 'Drafts', color: 'violet' }],
-    content: howToUpdateDraftPropertiesGuide,
   },
   {
     id: uuidv4(),
