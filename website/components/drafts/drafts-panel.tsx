6a2b50a7019dc22900e55965abafe0002a09eb7c
Veljko <radisavljevicveljko123@gmail.com>
2026-08-30 13:42:02 +0200

    change new draft card enter animation duration in drafts panel

--- website/components/drafts/drafts-panel.tsx
+++ website/components/drafts/drafts-panel.tsx
@@ -54,7 +54,7 @@ export default function DraftsPanel({
           {isNewDraftVisible && (
             <motion.li
               initial={{ opacity: 0, scale: 0.95 }}
-              animate={{ opacity: 1, scale: 1 }}
+              animate={{ opacity: 1, scale: 1, transition: {duration: 0.35} }}
             >
               <DraftCard
                 draft={{
