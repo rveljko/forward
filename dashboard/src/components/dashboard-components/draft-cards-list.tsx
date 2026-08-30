151ca79ad1d6731db4c3d6921abb61307132e3a7
Veljko <radisavljevicveljko123@gmail.com>
2026-08-30 13:30:22 +0200

    change draft card enter animation duration in draft cards list

--- dashboard/src/components/dashboard-components/draft-cards-list.tsx
+++ dashboard/src/components/dashboard-components/draft-cards-list.tsx
@@ -27,7 +27,7 @@ export default function DraftCardsList({
             layout="position"
             key={draft.id}
             initial={{ opacity: 0, scale: 0.95 }}
-            animate={{ opacity: 1, scale: 1 }}
+            animate={{ opacity: 1, scale: 1, transition: { duration: 0.35 } }}
             exit={{ opacity: 0, scale: 0.95 }}
           >
             <DraftCard draft={draft} />
