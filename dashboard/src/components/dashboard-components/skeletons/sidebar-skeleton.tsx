42185d0cd4aaf0729e5bc3318ccf4a7103af6d09
Veljko <radisavljevicveljko123@gmail.com>
2026-09-02 13:17:40 +0200

    hide logo in sidebar skeleton on small screens

--- dashboard/src/components/dashboard-components/skeletons/sidebar-skeleton.tsx
+++ dashboard/src/components/dashboard-components/skeletons/sidebar-skeleton.tsx
@@ -2,28 +2,28 @@ import Skeleton from '@dashboard-components/ui/skeleton'
 
 export default function SidebarSkeleton() {
   return (
-    <aside className="h-full w-15.5 shrink-0 p-4 md:w-64">
+    <aside className="h-full w-16 shrink-0 p-4 md:w-64">
       <div className="flex h-full flex-col gap-4">
         <div className="flex items-center justify-between gap-2 max-md:flex-col">
-          <Skeleton className="max-md:size-5 max-md:rounded-full md:h-7 md:max-w-25" />
+          <Skeleton className="hidden h-7 max-w-25 md:block" />
           <Skeleton className="size-7" />
         </div>
         <div className="flex h-full flex-col gap-1 overflow-y-auto">
           <div className="space-y-1">
-            <Skeleton className="max-md:size-7 md:h-7" />
-            <Skeleton className="max-md:size-7 md:h-7" />
+            <Skeleton className="size-7 md:h-7 md:w-auto" />
+            <Skeleton className="size-7 md:h-7 md:w-auto" />
           </div>
           <div className="flex h-full flex-col justify-between gap-1">
             <div className="space-y-1">
-              <Skeleton className="max-md:size-7 md:h-7" />
-              <Skeleton className="max-md:size-7 md:h-7" />
-              <Skeleton className="max-md:size-7 md:h-7" />
+              <Skeleton className="size-7 md:h-7 md:w-auto" />
+              <Skeleton className="size-7 md:h-7 md:w-auto" />
+              <Skeleton className="size-7 md:h-7 md:w-auto" />
             </div>
             <div className="space-y-1">
-              <Skeleton className="max-md:size-7 md:h-7" />
-              <Skeleton className="max-md:size-7 md:h-7" />
-              <Skeleton className="max-md:size-7 md:h-7" />
-              <Skeleton className="max-md:size-7 md:h-7" />
+              <Skeleton className="size-7 md:h-7 md:w-auto" />
+              <Skeleton className="size-7 md:h-7 md:w-auto" />
+              <Skeleton className="size-7 md:h-7 md:w-auto" />
+              <Skeleton className="size-7 md:h-7 md:w-auto" />
             </div>
           </div>
         </div>
